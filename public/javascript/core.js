28f126308b9394543448e3d9ded745da3172c7bf
Richard <richard@memespring.co.uk>
2011-04-20 13:38:21 +0100

    rejigged css/html/js for splash and about pages

--- public/javascript/core.js
+++ public/javascript/core.js
@@ -1,3 +1,4 @@
+//Reusable functions
 var Alphagov = {
   read_cookie: function(name) {
     var cookieValue = null;
@@ -26,6 +27,7 @@ var Alphagov = {
   }
 }
 
+//General page setup
 jQuery(document).ready(function() {
 
   //Setup annotator links 
@@ -48,7 +50,7 @@ jQuery(document).ready(function() {
       $('#tour-close').click(close_tour);
       $('#splash-back').fadeIn();
     });
-    return false
+    return false;
   }
 
   var close_tour = function() {
@@ -56,9 +58,62 @@ jQuery(document).ready(function() {
     return false;
   }
 
+  //setup tour click event
   $('#tour-launcher').click(launch_tour);
+  
+  //set initial cookie ?
   if (has_no_tour_cookie()) {
     write_tour_coookie();
     launch_tour();
   }
+});
+
+//Tour setup
+$(document).bind('tour-ready', function () {
+
+    var $panels = $('#slider .scrollContainer > div');
+    var $container = $('#slider .scrollContainer');
+
+    // if false, we'll float all the panels left and fix the width 
+    // of the container
+    var horizontal = true;
+
+    // collect the scroll object, at the same time apply the hidden overflow
+    // to remove the default scrollbars that will appear
+    var $scroll = $('#slider .scroll').css('overflow', 'hidden');
+
+    // handle nav selection
+    function selectNav() {
+        $(this)
+            .parents('ul:first')
+                .find('a')
+                    .removeClass('selected')
+                .end()
+            .end()
+            .addClass('selected');
+    }
+
+    $('#slider .navigation').find('a').click(selectNav);
+
+    // go find the navigation link that has this target and select the nav
+    function trigger(data) {
+        var el = $('#slider .navigation').find('a[href$="' + data.id + '"]').get(0);
+        selectNav.call(el);
+    }
+
+    //setup scroll optioons
+    var scrollOptions = {
+        target: $scroll, // the element that has the overflow
+        items: $panels,
+        navigation: '.navigation a',
+        axis: 'xy',
+        onAfter: trigger, // our final callback
+        offset: 0,
+        duration: 500,
+    };
+
+    //set first item selected and then setup scroll
+    $('ul.navigation a:first').click();    
+    $.localScroll(scrollOptions);
+
 });
--- public/javascript/jquery.slider.js
+++ public/javascript/jquery.slider.js
@@ -8,17 +8,6 @@ $(document).bind('tour-ready', function () {
     // of the container
     var horizontal = true;
 
-    // float the panels left if we're going horizontal
-    if (horizontal) {
-        $panels.css({
-            'float' : 'left',
-            'position' : 'relative' // IE fix to ensure overflow is hidden
-        });
-
-        // calculate a new width for the container (so it holds all panels)
-        $container.css('width', $panels[0].offsetWidth * $panels.length);
-    }
-
     // collect the scroll object, at the same time apply the hidden overflow
     // to remove the default scrollbars that will appear
     var $scroll = $('#slider .scroll').css('overflow', 'hidden');
@@ -48,52 +37,19 @@ $(document).bind('tour-ready', function () {
         $('ul.navigation a:first').click();
     }
 
-    // offset is used to move to *exactly* the right place, since I'm using
-    // padding on my example, I need to subtract the amount of padding to
-    // the offset.  Try removing this to get a good idea of the effect
-    var offset = parseInt((horizontal ? 
-        $container.css('paddingTop') : 
-        $container.css('paddingLeft')) 
-        || 0) * -1;
-
-
+    //setup scroll optioon
     var scrollOptions = {
         target: $scroll, // the element that has the overflow
-
-        // can be a selector which will be relative to the target
         items: $panels,
-
         navigation: '.navigation a',
-
-        // allow the scroll effect to run both directions
         axis: 'xy',
-
         onAfter: trigger, // our final callback
-
-        offset: offset,
-
-        // duration of the sliding effect
+        offset: 0,
         duration: 500,
-
-        // easing - can be used with the easing plugin: 
-        // http://gsgd.co.uk/sandbox/jquery/easing/
-        easing: 'swing'
     };
 
-    // apply serialScroll to the slider - we chose this plugin because it 
-    // supports// the indexed next and previous scroll along with hooking 
-    // in to our navigation.
-    $('#slider').serialScroll(scrollOptions);
-
-    // now apply localScroll to hook any other arbitrary links to trigger 
-    // the effect
+    //do the scroll
     $.localScroll(scrollOptions);
 
-    // finally, if the URL has a hash, move the slider in to position, 
-    // setting the duration to 1 because I don't want it to scroll in the
-    // very first page load.  We don't always need this, but it ensures
-    // the positioning is absolutely spot on when the pages loads.
-    scrollOptions.duration = 1;
-    $.localScroll.hash(scrollOptions);
 
 });
