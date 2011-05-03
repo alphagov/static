10cc9ecb7a9c4ec9f3df683078a276a9641c5ffd
Matt Patterson <matt@reprocessed.org>
2011-05-03 17:55:55 +0100

    Add global fixed-position error container; improve error handling in locate_me.js and geo.js

--- public/javascript/locate_me.js
+++ public/javascript/locate_me.js
@@ -10,10 +10,11 @@
   /**
    * $ is an alias to jQuery object
    */
-  $.fn.locator = function(ignore_location_known_on_page_load) {
+  $.fn.locator = function(ignore_location_known_on_page_load, error_area_selector) {
     var locator_form = this.closest('form'),
         locator_box = this,
         ignore_location_known_on_page_load = ignore_location_known_on_page_load || false,
+        error_area_selector = error_area_selector || '#global-app-error',
         ask_ui = locator_box.find('.ask_location'),
         locating_ui = locator_box.find('.finding_location'),
         found_ui = locator_box.find('.found_location'),
@@ -25,42 +26,47 @@
       var geolocation_ui_node = ask_ui.find('.locate-me');
       if (geolocation_ui_node.length > 0) return geolocation_ui_node;
       return $('<p class="locate-me">or <a href="#">locate me automatically</a></p>').appendTo(ask_ui);
-    }
+    };
     var show_ui = function(ui_to_show) {
       all_ui.addClass('hidden');
       ui_to_show.removeClass('hidden');
-    }
+    };
     var update_geo_labels = function(geo_data) {
       found_ui.find('h3').text(geo_data.locality);
       found_ui.find('a').text('Not in ' + geo_data.locality + '?');
-    }
+    };
     var update_geo_fields = function(geo_data) {
       locator_box.find('input[name=lat]').val(geo_data.lat);
       locator_box.find('input[name=lon]').val(geo_data.lon);
-
-    }
+    };
+    var clear_geo_fields = function() {
+      locator_box.find('input[name=lat]').val('');
+      locator_box.find('input[name=lon]').val('');
+    };
     var dispatch_location = function(response_data) {
-      $(document).trigger('location-changed', response_data);
+      if (response_data.current_location != undefined) {
+        $(error_area_selector).empty().addClass("hidden");
+        $(document).trigger('location-changed', response_data);
+      }
+      else {
+        $(error_area_selector).empty().append("<p>Sorry, we couldn't find a match for that postcode. Please check you entered it correctly. If the postcode was correct, try the Automatically Locate Me button.</p>").removeClass('hidden');
+        show_ui(ask_ui);
+      }
     }
     var changed_location = function(data) {
-      if (data.current_location.lat != '0.0') {
+      if (data.current_location != undefined) {
         update_geo_labels(data.current_location);
         update_geo_fields(data.current_location);
         show_ui(found_ui);
         locator_box.data('located', true);
       }
       else {
-        if (ask_ui.find('.error-response').length == 0) {
-          ask_ui.prepend('<p class="error-response"></p>');
-        }
-        ask_ui.find('.error-response').text("Sorry, we couldn't find a match for that postcode. Please check you entered it correctly.  If the postcode was correct, try the Automatically Locate Me button.");
         show_ui(ask_ui);
         locator_box.data('located', false);
       }
     }
     var reset_location = function() {
-      locator_box.find('input[name=lat]').val('');
-      locator_box.find('input[name=lon]').val('');
+      clear_geo_fields();
       found_ui.find('h3').text('');
       show_ui(ask_ui);
       locator_box.data('located', false);
@@ -84,7 +90,7 @@
       });
       geolocate_ui.bind('location-completed', function (event, details) {
         update_geo_fields(details);
-        locator_form.trigger('submit');
+        $.post(this.action, locator_form.serialize(), dispatch_location, 'json');
       });
       geolocate_ui.find('a').click(function (e) {
         var parent_element = $(this).closest('.locate-me');
@@ -124,6 +130,7 @@
     });
     locator_form.submit(function(e) {
       e.preventDefault();
+      clear_geo_fields();
       $.post(this.action, locator_form.serialize(), dispatch_location, 'json');
       show_ui(locating_ui);
     });
