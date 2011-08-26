0b894ac78e2f206dbdf94475806c59954bb606cd
Frances Berriman <phaeness@Phae.local>
2011-08-26 14:07:00 +0100

    Shortened script for step nav

--- public/javascripts/programmes.js
+++ public/javascripts/programmes.js
@@ -9,15 +9,18 @@ $(function(){
 		i = sections.length,
 		last = $(".tabs-panel:last").attr("id").split("-enhanced")[0],
 		j, 
-		id;
+		id,
+		ul,
+		sectionTitle,
+		nav;
 		
 		last = "<li><a href='#"+last+"'>No thanks, just tell me how to claim</a></li>"
 		
 	while(i--){
-		var j = i+1,
-		nav = $("<nav class='part-pagination group' role='navigation'></nav>"),
-		ul = $("<ul></ul>"),
-		id = $(sections[j]).attr("id"),
+		j = i+1;
+		nav = $("<nav class='part-pagination group' role='navigation'></nav>");
+		ul = $("<ul></ul>");
+		id = $(sections[j]).attr("id");
 		sectionTitle = $("#"+id+" h1").html();
 
 		if(sectionTitle != undefined){
