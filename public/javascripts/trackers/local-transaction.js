50b373e97d946467a2d2eae6b1966b646c97b135
Frances Berriman <fberriman@gmail.com>
2011-11-28 13:09:41 +0000

    Local transcation tracker

--- public/javascripts/trackers/local-transaction.js
+++ public/javascripts/trackers/local-transaction.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // click on 'get started' CTA
   $(".get-started a").click(function(){
-    _gaq.push(['_trackEvent', 'Citizen-Format-Service', 'Success-interaction']);
+    _gaq.push(['_trackEvent', 'Citizen-Format-Localservice', 'Success-interaction']);
     return true;
   })
   
