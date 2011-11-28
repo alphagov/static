c3239a5832c124645a661d7debed1ff6335158c7
Frances Berriman <fberriman@gmail.com>
2011-11-28 12:22:55 +0000

    Benefit tracking

---
+++ public/javascripts/trackers/benefit.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // click on link in benefit (programme)
+  $(".article-container a").click(function(){
+    _gaq.push(['_trackEvent', 'Citizen-Format-Benefit', 'Success-interaction']);
+    return true;
+  })
+  
+  // on a benefit (programme) page for 10 seconds
+  setTimeout("_gaq.push(['_trackEvent', 'Citizen-Format-Benefit', 'Success-duration']);",10000);
+});
