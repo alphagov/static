ba45f7787626070f4a5b4a545046a0feed63abd7
Frances Berriman <fberriman@gmail.com>
2011-11-28 12:13:26 +0000

    Guide tracking

---
+++ public/javascripts/trackers/guide.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // click on link in guide
+  $(".article-container a").click(function(){
+    _gaq.push(['_trackEvent', 'Citizen-Format-Guide', 'Success-interaction']);
+    return true;
+  })
+  
+  // on a guide page for 10 seconds
+  setTimeout("_gaq.push(['_trackEvent', 'Citizen-Format-Guide', 'Success-duration']);",10000);
+});
