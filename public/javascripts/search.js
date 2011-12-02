70d7844ddbc8011a4016595e7c63cbe5b2c2fc29
Frances Berriman <fberriman@gmail.com>
2011-12-02 15:28:22 +0000

    Autosuggest hits JSON feed now

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -17,38 +17,38 @@ $(document).ready(function() {
     }
   });
 	
-  $.ajax({
-    url: "/autocomplete?q=",
-    isLocal:true,
-    dataType: "json",
-    cache: true,
-    success: function(data) {
-      var results = $.map(data, function(e) {
-        return { 'label': e.title, 'url': "/"+e.link, 'class': e.format };
+  $("#site-search-text").autocomplete({ 
+    delay: 300,
+    width: 300, 
+    source: function(req, add){  
+      $.ajax({
+        url: "/autocomplete?q="+req.term,
+        dataType: "json",
+        cache: true,
+        success: function(data) {
+          var results = $.map(data, function(e) {
+            return { 'label': e.title, 'url': "/"+e.link, 'class': e.format };
+          });
+          add(results)
+        }
       });
-      $('#site-search-text').autocomplete({
-        delay: 0,
-				width:300,
-        source: results, 
-        select: function(event, ui) {
-          location.href = ui.item.link;
-        },
-				open: function(event, ui){
-					// all this just to move the ul to the left by an offset
-					var offset = $("#site-search-text").offset(),
-						leftoffset = offset.left,
-						width = $("#site-search-text").width();
-					
-					var newLeft = (leftoffset - width);
-					$(".ui-autocomplete").css("left", newLeft+"px").css("width", ((width*2)+4)+"px");
-					
-					// quickly add the search value to end of list
-					var searchVal = $("#site-search-text").attr("value");
-					$(".ui-autocomplete").append("<li class='search-site ui-state-hover'><a href='/search?q="+searchVal+"' class='ui-corner-all' tabindex='-1'>Search for <em>"+searchVal+"</em></li>");
-					}
-      });
+    },  
+    select: function(event, ui) {
+      location.href = ui.item.link;
+    },
+    open: function(event, ui){
+      // all this just to move the ul to the left by an offset
+      var offset = $("#site-search-text").offset(),
+      leftoffset = offset.left,
+      width = $("#site-search-text").width();
 
-    }
-		
+      var newLeft = (leftoffset - width);
+      $(".ui-autocomplete").css("left", newLeft+"px").css("width", ((width*2)+4)+"px");
+
+      // quickly add the search value to end of list
+      var searchVal = $("#site-search-text").attr("value");
+      $(".ui-autocomplete").append("<li class='search-site ui-state-hover'><a href='/search?q="+searchVal+"' class='ui-corner-all' tabindex='-1'>Search for <em>"+searchVal+"</em></li>");
+    } 
   });
+  
 });
