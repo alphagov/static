20cd8ee63ec98d4fd5612096498717c5c00e8077
Frances Berriman <fberriman@gmail.com>
2012-01-09 14:22:25 +0000

    Dismiss feedback if cookie set

--- public/javascripts/feedback.js
+++ public/javascripts/feedback.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  
 	var selects = $("#govuk-feedback #feedback-options").html();
 	
 	var html = "<div id='feedback-cta' class='left'><h2>Helpful?</h2><p><a href='#' class='close' id='feedback-dismiss' title='Close'>x</a></p<form><input id='cta-yes' type='button' value='Yes' /><input id='cta-no' type='button' value='No' /></form></div>";
@@ -11,6 +12,7 @@ $(document).ready(function() {
 	  _gaq.push(['_trackEvent', 'Citizen-Feedback', 'Yes']);
 	  $("#feedback-cta").html("<h2>Thanks for letting us know</h2>");
 	  $("#feedback-cta").delay(1500).fadeOut('slow');
+	  setCookie("govukfeedback","dismiss",7);
 	  // set cookie to dismiss it for good
 	})
   $("#cta-no").click(function(){
@@ -21,7 +23,7 @@ $(document).ready(function() {
     $("#feedback-type").live("change", function(){
       
 			var id = $(this).find('option:selected').attr('id');
-			console.log(id)
+
 			switch(id)
 			{
 			case "policy":
@@ -43,11 +45,38 @@ $(document).ready(function() {
 			  $("#popup #feedback-mechanism").html();
 			}
 			
+			setCookie("govukfeedback","dismiss",7)
 		});
   });
   $("#feedback-dismiss").click(function(){
     $("#feedback-cta").remove();
     _gaq.push(['_trackEvent', 'Citizen-Feedback', 'Dismiss']);
+    setCookie("govukfeedback","dismiss",7);
   })
-  $("#feedback-cta").delay(30000).fadeIn(1500);
+  
+  if(getCookie("govukfeedback") != "dismiss"){
+    $("#feedback-cta").delay(30000).fadeIn(1500);
+  }
+  
+  function setCookie(name,value,days) {
+      if (days) {
+          var date = new Date();
+          date.setTime(date.getTime()+(days*24*60*60*1000));
+          var expires = "; expires="+date.toGMTString();
+      }
+      else var expires = "";
+      document.cookie = name+"="+value+expires+"; path=/";
+  }
+
+  function getCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0;i < ca.length;i++) {
+          var c = ca[i];
+          while (c.charAt(0)==' ') c = c.substring(1,c.length);
+          if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
+  }
+
 });
