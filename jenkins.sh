3e0c29e3476de411ad24aa71fc0f3feab4dbec83
James Adam <james@lazyatom.com>
2012-01-18 16:26:13 +0000

    Concensus says we don't care about CSSLint.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -x
-csslint public/stylesheets
-echo "Not failing on csslint failures"
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 RAILS_ENV=test bundle exec rake test --trace
