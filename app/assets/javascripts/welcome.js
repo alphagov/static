d95a13f9ebf98b1623845fbcf1b2dd09b6131dd8
Frances Berriman <fberriman@gmail.com>
2012-01-24 12:05:13 +0000

    Load extra images via JS

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -33,8 +33,15 @@ $(function() {
     return;
   })
   
-
+  
   if($(".carousel").length != 0){
+    
+    $(preloadImages).each(function() {
+    	var item = $('<img />').attr('src',this).appendTo("<li></li>");
+      item.parent().appendTo($(".carousel ul"));
+    });
+    
+    
     $(".carousel").jCarouselLite({
       auto: 7000,
       visible: 1,
