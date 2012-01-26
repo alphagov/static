b066191e174e92aeb16766b6be3d2341fafcf8da
Paul Battley <pbattley@gmail.com>
2012-01-26 11:49:53 +0000

    Delinting.

--- app/assets/javascripts/customisation-settings.js
+++ app/assets/javascripts/customisation-settings.js
@@ -125,12 +125,14 @@ $(document).ready(function() {
       }
   }*/
   function setCookie(name,value,days) {
+    var expires;
     if (days) {
       var date = new Date();
       date.setTime(date.getTime()+(days*24*60*60*1000));
-      var expires = "; expires="+date.toGMTString();
+      expires = "; expires="+date.toGMTString();
+    } else {
+      expires = "";
     }
-    else var expires = "";
     document.cookie = name+"="+value+expires+"; path=/";
   }
 
@@ -140,7 +142,7 @@ $(document).ready(function() {
     for(var i=0;i < ca.length;i++) {
       var c = ca[i];
       while (c.charAt(0)==' ') c = c.substring(1,c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
     }
     return null;
   }
