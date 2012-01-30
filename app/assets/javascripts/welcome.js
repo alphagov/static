853882becfcf5ed29393e58ba5338cc6ad5e95f5
Frances Berriman <fberriman@gmail.com>
2012-01-30 10:10:27 +0000

    Stripping back homepage

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -69,18 +69,6 @@ $(function() {
     });
 
     $(".header-global h1 a").focus();
-  };
-  
-  if($(".carousel").length !== 0) {
-    $(preloadImages).each(function(){
-      var item = $('<img />').attr('src', this).appendTo("<li></li>");
-      item.parent().appendTo($(".carousel ul"));
-    });
-    
-    $(".carousel").jCarouselLite({
-      auto: 7000,
-      visible: 1,
-      speed: 600
-    });
+  };  
   }
 });
