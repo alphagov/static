cbc33d370dee421129a48c66490b45b659dc3539
Matthew Wall <mwall@alphagov.co.uk>
2012-01-31 12:16:27 +0000

    Top trumps for search autocomplete

---
+++ app/assets/javascripts/search-top-trumps.js
@@ -0,0 +1,17 @@
+if (typeof(GDS) == 'undefined') { GDS = {}; };
+GDS.search_top_trumps = [
+	{
+		"title": "Do important stuff with car tax",
+		"link":"/when-do-the-clocks-change",
+		"format":"answer",
+		"keywords": "car tax"
+		"weight": 3
+	},
+	{
+		"title": "Other, less important things about cars"
+		"link":"/when-do-the-clocks-change",
+		"format":"answer",
+		"keywords": "car tax"
+		"weight": 1
+	}
+];
