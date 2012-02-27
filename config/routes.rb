acab5d02967cf99d26974ebe9bb095bf72a9b003
Jordan Hatch <jordan@jordanh.net>
2012-02-27 16:04:38 +0000

    Add 406 error to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Static::Application.routes.draw do
   match "/templates/homepage.html.erb", :to => "root#homepage"
   match "/templates/admin.html.erb", :to => "root#admin"
   match "/templates/404.html.erb", :to => "root#404"
+  match "/templates/406.html.erb", :to => "root#406"
   match "/templates/418.html.erb", :to => "root#418"
   match "/templates/500.html.erb", :to => "root#500"
   match "/templates/501.html.erb", :to => "root#501"
