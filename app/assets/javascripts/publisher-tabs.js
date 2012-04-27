ae0bdcc4906b3a11ca9b87862d51e312077de4e6
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2012-04-27 11:09:41 +0100

    Trim out gurt big chunk of commented-out tab JS.

--- app/assets/javascripts/publisher-tabs.js
+++ app/assets/javascripts/publisher-tabs.js
@@ -37,38 +37,5 @@ $(function() {
     }
   }
 
-  // if ('pushState' in window.history) {
-  //   // Define our own click handler for the tabs, overriding the default.
-  //   tab_options['event'] = 'change';
-  //   tabbable.tabs(tab_options);
-  //
-  //   tabbable.delegate('ul.ui-tabs-nav a', 'click', function() {
-  //     var the_element = $(this),
-  //       state = {},
-  //       // Get the id of this tab widget.
-  //       id = the_element.closest( '.tabs' ).attr('id'),
-  //       title = the_element.text(),
-  //       anchor = the_element.attr('href'),
-  //       // Get the index of this tab.
-  //       idx = $(this).parent().prevAll().length;
-  //
-  //     // Set the state!
-  //     state[id] = idx;
-  //     window.history.pushState(state, title, document.location.pathname + document.location.search + anchor);
-  //     $(window).trigger('hashchange');
-  //   });
-  //
-  //   // Bind an event to window.onhashchange that, when the history state changes,
-  //   // change the current tab as necessary.
-  //   $(window).bind('hashchange', function(e) {
-  //     correct = tabbable.find('ul.ui-tabs-nav a[href=' + document.location.hash + ']');
-  //     correct.triggerHandler('change');
-  //   })
-  //
-  //   // Since the event is only triggered when the hash changes, we need to trigger
-  //   // the event now, to handle the hash the page may have loaded with.
-  //   $(window).trigger('hashchange');
-  // } else {
-    tabbable.tabs(tab_options);
-  // }
+  tabbable.tabs(tab_options);
 });
