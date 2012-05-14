e6277c3343699871ed79615c1755cb080ada6aa5
Alex Torrance <alex.torrance@digital.cabinet-office.gov.uk>
2012-05-14 07:10:49 -0700

    Delayed dislay of Feedback CTA
    CTA div is created with 'hide' class
    setTimeout to remove 'hide' after 3 secs
    further setTimeout to add 'hide' class after 5 additional secs
    Moved transition rules to #feedback-cta block

--- app/assets/javascripts/feedback.js
+++ app/assets/javascripts/feedback.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   var popupContents;
   
-	$("#cta-no").live("click", function(){
-	  // send to a collection bucket
-	  _gaq.push(['_trackEvent', 'Citizen-Feedback', 'No']);
-	  $("#feedback-cta").html("<h2>Feedback</h2><p>Thanks for letting us know</p>");
-	  //$("#feedback-cta").delay(1500).fadeOut('slow');
-	  setCookie("govukfeedback","dismiss",7);
-	})
+  $("#cta-no").live("click", function(){
+    // send to a collection bucket
+    _gaq.push(['_trackEvent', 'Citizen-Feedback', 'No']);
+    $("#feedback-cta").html("<h2>Feedback</h2><p>Thanks for letting us know</p>");
+    //$("#feedback-cta").delay(1500).fadeOut('slow');
+    setCookie("govukfeedback","dismiss",7);
+  })
   $("#cta-yes").live("click", function(){
     _gaq.push(['_trackEvent', 'Citizen-Feedback', "Yes"]);
     BetaPopup.popup(popupContents, "feedback-tools");
     $("#popup h2").focus;
     //$("#feedback-cta").fadeOut('fast');
     
-		$("#popup form").live("submit", function(){
-		  $.ajax({
+    $("#popup form").live("submit", function(){
+      $.ajax({
         type: 'GET',
         url: this.action,
         data: $(this).serialize(),
@@ -23,10 +23,10 @@ $(document).ready(function() {
           $("#popup form").html("<p class='feedback-thanks'>Thanks for your feedback</p><p>We'll send this feedback to the team.</p>")
         }
       });
-		  return false;
-		})	
-		
-		setCookie("govukfeedback","dismiss",7);
+      return false;
+    })  
+    
+    setCookie("govukfeedback","dismiss",7);
   });
   /*$("#feedback-dismiss").live("click", function(){
    // $("#feedback-cta").addClass("hide");
@@ -36,21 +36,25 @@ $(document).ready(function() {
   })*/
   
   if($("meta[name=x-section-format]").length != 0){
-    setUpFeedback()
+    setUpFeedback();
     if(getCookie("govukfeedback") == "dismiss"){
       $("#feedback-cta").addClass("hide"); 
     }
-    else{
-      setTimeout('$("#feedback-cta").addClass("hide");',5000);
-    }
+    // else{
+    //   setTimeout('$("#feedback-cta").addClass("hide");',5000);
+    // }
   }
   
   function setUpFeedback(){
-    var html = "<div id='feedback-cta' class='left'><div class='inner'><h2>Feedback</h2><p>Can this page be better?</p><form><input id='cta-yes' type='button' value='Yes' /><input id='cta-no' type='button' value='No' /><p class='sets-cookie'><a href='/help/cookies#feedbackcookies' title='These buttons set a feedback cookie'>Sets a cookie</a></p></form></div></div>";
-  	$("#entry_3").val(location.href);
+    var html = "<div id='feedback-cta' class='left hide'><div class='inner'><h2>Feedback</h2><p>Can this page be better?</p><form><input id='cta-yes' type='button' value='Yes' /><input id='cta-no' type='button' value='No' /><p class='sets-cookie'><a href='/help/cookies#feedbackcookies' title='These buttons set a feedback cookie'>Sets a cookie</a></p></form></div></div>";
+    $("#entry_3").val(location.href);
 
-  	popupContents = $("#govuk-feedback").html();
-  	$("body").append(html);
+    popupContents = $("#govuk-feedback").html();
+    $("body").append(html);
+    setTimeout(function() {
+      $("#feedback-cta").removeClass("hide");
+      setTimeout('$("#feedback-cta").addClass("hide");',5000);
+    },3000)
   }
   
   function setCookie(name,value,days) {
