888f8ae9e3d848155768cacf6585801218a2a72e
Alex Torrance <alex.torrance@digital.cabinet-office.gov.uk>
2012-07-04 14:55:12 +0100

    Fixes #1895. Cleaned up pop up js and css
    Mask element is now fixed position and set to 100% height & width
    Cleaned up popup.js code
    Removed duplicate code from customisation-settings.js

--- app/assets/javascripts/popup.js
+++ app/assets/javascripts/popup.js
@@ -21,45 +21,36 @@ var BetaPopup = {
   */
 
   popup: function(html, ident, source){
-    var source = source || "#header-global h1 a";
-    $("body").append("<div id='mask'></div>");
-    $("body").append("<div id='popup' class="+ident+"></div>");
-    //$("#popup").append("<p class='close'><a href='#'>Close</a></p>")
-    $("#popup").append(html);
+    $("body").append("<div id='mask'></div>").append("<div id='popup' class="+ident+"></div>");
 
-    //Get the screen height and width
-    var maskHeight = $(document).height();
-    var maskWidth = $(window).width();
+    var source = source || "#header-global h1 a",
+    $popup = $('#popup'), $win = $(window), $mask = $('#mask');
+    $popup.append(html);
 
-    //Set heigth and width to mask to fill up the whole screen
-    $('#mask').css({'width':maskWidth,'height':maskHeight});
-     
-    $('#mask').fadeTo("fast",0.6);  
+    $mask.fadeTo("fast",0.6);  
 
     //Get the window height and width
-    var winH = $(window).height();
-    var winW = $(window).width();
+    var winH = $win.height();
+    var winW = $win.width();
 
     //Set the popup window to center
-    $("#popup").css('left', winW/2-$("#popup").width()/2);
+    $popup.css('left', winW/2-$popup.width()/2);
 
-    $("#popup").delay(100).fadeIn('fast', function(){
-      $("#popup h2").attr("tabindex",-1).focus();
+    $popup.delay(100).fadeIn('fast', function(){
+      $popup.find("h2").attr("tabindex",-1).focus();
     });
-    $(".close").live('keypress', function (e) {
-       if ( e.keyCode == 27 ){
+    $popup.on('keydown', function (e) {
+       if ( e.which == 27 ){
            closePopup();
         }
-    });
-    $(".close").live("click", function(){
+    }).on("click", ".close", function(){
       closePopup()
       return false;
     });
+    
     closePopup = function(){
-      $("#popup").fadeOut(400, function(){
-        $("#mask").slideUp('fast');
-        $("#mask").remove();
-        $("#popup").remove();
+      $popup.fadeOut(400, function(){
+        $mask.slideUp('fast', function() { $(this).remove(); $popup.remove(); });
       });
 
       $(source).focus();
