2e97cc60b45c54fe0ca95552398702cc34f18247
Alex Torrance <hello@alextorrance.co.uk>
2012-07-16 14:52:19 +0100

    fix popup mask styles for ie6

--- app/assets/javascripts/popup.js
+++ app/assets/javascripts/popup.js
@@ -21,32 +21,29 @@ var BetaPopup = {
   */
 
   popup: function(html, ident, source){
-    $("body").append("<div id='mask'></div>").append("<div id='popup' class="+ident+"></div>");
+    $("body").append("<div id='mask' class='popover-mask'></div>").append("<div id='popup' class="+ident+"></div>");
 
     var source = source || "#header-global h1 a",
     $popup = $('#popup'), $win = $(window), $mask = $('#mask');
-    $popup.append(html);
-
-    $mask.fadeTo("fast",0.6);  
-
     //Get the window height and width
-    var winH = $win.height();
-    var winW = $win.width();
+    var winH = $win.height(), winW = $win.width();
+
+    $popup.append(html);
+    $mask.fadeTo("fast",0.6).height(winH).width(winW);
 
     //Set the popup window to center
-    $popup.css('left', winW/2-$popup.width()/2);
-
-    $popup.delay(100).fadeIn('fast', function(){
-      $popup.find("h2").attr("tabindex",-1).focus();
-    });
-    $popup.on('keydown', function (e) {
-       if ( e.which == 27 ){
-           closePopup();
-        }
-    }).on("click", ".close", function(){
-      closePopup()
-      return false;
-    });
+    $popup.css('left', winW/2-$popup.width()/2)
+      .delay(100).fadeIn('fast', function(){
+        $popup.find("h2").attr("tabindex",-1).focus();
+      })
+      .on('keydown', function (e) {
+         if ( e.which == 27 ){
+             closePopup();
+          }
+      }).on("click", ".close", function(){
+        closePopup()
+        return false;
+      });
     
     closePopup = function(){
       $popup.fadeOut(400, function(){
