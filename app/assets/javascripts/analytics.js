151ae1ccd97513c14b67e1036b5a03682379e549
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-07-25 15:31:18 +0100

    Add Google Analytics tracking for Guide success.
    As the page is loading an custom event is fired to say that it was entered.
    On DOM ready the success tracking function is set up. This sends custom
    events for Success, Success due to being on the site for more than seven
    seconds and Success due to clicking to another page on the guide.

---
+++ app/assets/javascripts/analytics.js
@@ -0,0 +1,56 @@
+(function() {
+    function trackTaskCompletion(needID, format) {
+        if (format == "Guide") {
+            trackGuideFormatSuccess(needID);
+        }
+    }
+
+    function trackFormatEntry(needID, format)
+    {
+        _gaq.push(['_trackEvent', 'MS_'+format, needID, 'Entry']);
+    }
+
+    function trackGuideFormatSuccess(needID)
+    {
+        var success = { "success": false };
+        setTimeout(function() {
+            onSuccess("Duration", success, needID);
+        }, 7000);
+        $("#content a").click(function() {
+            onSuccess("Interaction", success, needID);
+            try {
+                setTimeout('document.location = "' + $(this).attr("href") + '"', 50)
+            }catch(err){}
+            return false;
+        });
+    }
+
+    function onSuccess(string, dict, needID) {
+        if (dict.success) {
+            return;
+        }
+        dict.success = true;
+        _gaq.push(['_trackEvent', 'MS_Guide', needID, 'Success-' + string]);
+        _gaq.push(['_trackEvent', 'MS_Guide', needID, 'Success']);
+    }
+
+    function userCameFromThePageWithinTheSameArtefact() {
+        var artefactURL = document.URL.split('/').slice(0,4).join('/');
+        return (document.referrer.indexOf(artefactURL) == 0);
+    }
+
+
+    // Track format entry before DOM ready
+    var userFromSameArtefact = userCameFromThePageWithinTheSameArtefact();
+    if (!userFromSameArtefact) {
+        trackFormatEntry(GOVUK.Analytics.NeedID, GOVUK.Analytics.Format);
+    }
+
+    // Track format success on DOM ready
+    $(function() {
+        if (!userFromSameArtefact) {
+            trackTaskCompletion(GOVUK.Analytics.NeedID, GOVUK.Analytics.Format);
+        }
+
+    });
+})();
