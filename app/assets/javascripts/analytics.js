d0fee1a2a942972bbab9d1b6d73a230fff6fc90f
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-09-18 17:25:45 +0100

    New event handling for link tracking
    Add new event handline for links which should not interfere with opening
    links in new tabs, or any other jquery click events.
    
    Author: @gtrogers
    Author: @robyoung

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -16,16 +16,61 @@ GOVUK.wireTrackingEvents = (function () {
 
     }
 
+    var successful = false;
+
+    function cacheEvent(event) {
+        var newEvent = {};
+        var target = event.target;
+        var shouldOpenInNewWindow = event.originalEvent.metaKey || event.originalEvent.ctrlKey;
+
+        if (event.originalEvent.initMouseEvent) {
+            newEvent = document.createEvent('MouseEvents');
+            newEvent.initMouseEvent(
+                event.originalEvent.type,
+                event.originalEvent.bubbles,
+                event.originalEvent.cancelable,
+                event.originalEvent.view,
+                event.originalEvent.detail,
+                event.originalEvent.screenX,
+                event.originalEvent.screenY,
+                event.originalEvent.clientX,
+                event.originalEvent.clientY,
+                event.originalEvent.ctrlKey,
+                event.originalEvent.altKey,
+                event.originalEvent.shiftKey,
+                event.originalEvent.metaKey,
+                event.originalEvent.button,
+                event.originalEvent.relatedTarget);
+        }
+
+        return {fire:function () {
+            if (event.originalEvent.initMouseEvent) {
+                target.dispatchEvent(newEvent);
+            } else {
+                if (shouldOpenInNewWindow) {
+                    $(target).attr('target', '_blank');
+                }
+                target.click();
+            }
+        }
+        };
+    }
+
     function interceptHyperLinkAndFireSuccess(selector, success) {
-        $(selector).click(function () {
+        $(selector).click(function (event) {
             onSuccess(success);
-            if ($(this).data('events').click === 1) {
-                var timeout = GOVUK.Analytics.debugMode ? 1000 : 50;
-                setTimeout('document.location = "' + $(this).attr("href") + '"', timeout);
+            if (!successful) {
+                successful = true;
+                var cachedEvent = cacheEvent(event);
+                event.stopImmediatePropagation();
                 event.preventDefault();
+                var delay = GOVUK.Analytics.debugMode ? 1000 : 50;
+                setTimeout(function () {
+                    cachedEvent.fire();
+                }, delay);
             }
         });
-    };
+    }
 
     function trackFormatEntry(needID, format) {
         GOVUK.sendToAnalytics(['_trackEvent', 'MS_' + format, needID, 'Entry']);
@@ -36,15 +81,15 @@ GOVUK.wireTrackingEvents = (function () {
         setTimeout(function () {
             onSuccess(success);
         }, 7000);
-        $(function(){
-            interceptHyperLinkAndFireSuccess("#content a",success);
+        $(function () {
+            interceptHyperLinkAndFireSuccess("#content a", success);
         });
     }
 
     function trackTransactionFormatSuccess() {
         var success = {"success":false};
         // should this actually be article?
-        $(function(){
+        $(function () {
             interceptHyperLinkAndFireSuccess(".article-container a", success);
         });
     }
