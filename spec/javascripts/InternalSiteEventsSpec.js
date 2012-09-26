586d941e5622e2ba20ae0a50830ea0873ad9c439
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-09-26 14:33:05 +0100

    Add tests for events cookie handler

---
+++ spec/javascripts/InternalSiteEventsSpec.js
@@ -0,0 +1,27 @@
+describe("internal site events", function() {
+    afterEach(function () {
+        GOVUK.Analytics.internalSiteEvents.sendAll();
+    });
+
+    it("should store an event in the cookie on push", function() {
+        GOVUK.Analytics.internalSiteEvents.push("event");
+
+        expect(Alphagov.read_cookie("successEvents"))
+            .toEqual(jQuery.base64Encode(JSON.stringify(["event"])));
+    });
+
+    it("should send the stored events to Google Analytics on sendAll", function() {
+        spyOn(GOVUK, 'sendToAnalytics');
+
+        GOVUK.Analytics.internalSiteEvents.push("event1");
+        GOVUK.Analytics.internalSiteEvents.push("event2");
+        GOVUK.Analytics.internalSiteEvents.sendAll();
+
+        var arguments = GOVUK.sendToAnalytics.argsForCall;
+        expect(arguments.length).toBe(2);
+        expect(arguments[0][0]).toBeEqualAsJSON("event1");
+        expect(arguments[1][0]).toBeEqualAsJSON("event2");
+        expect(Alphagov.read_cookie("successEvents"))
+            .toBe(null);
+    });
+});
