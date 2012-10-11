49ba130416f6b50faaa06a6df7b4605c5d85de71
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-10-11 14:50:00 +0000

    Show campaign notifications
    Campaign notifications should be outputted to the relevant view using
    the provided helpers.

--- test/integration/notifications_test.rb
+++ test/integration/notifications_test.rb
@@ -14,7 +14,7 @@ class NotificationsTest < ActionDispatch::IntegrationTest
           .returns('')
       end
 
-      should "should not show a banner notification on the page" do
+      should "not show a banner notification on the page" do
         visit "/templates/wrapper.html.erb"
         refute page.has_selector? "#banner-notification"
       end
@@ -57,6 +57,10 @@ class NotificationsTest < ActionDispatch::IntegrationTest
   end
 
   context "campaign notifications" do
+    setup do
+      NotificationFileLookup.campaign_file = nil
+    end
+
     context "given view files are empty" do
       setup do
         File.stubs(:read).with("#{Rails.root}/app/views/notifications/campaign_green.erb")
@@ -67,9 +71,60 @@ class NotificationsTest < ActionDispatch::IntegrationTest
           .returns('')
       end
 
-      should "should not show a campaign notification on the page" do
+      should "not show a campaign notification on the page" do
         visit "/templates/campaign.html.erb"
         refute page.has_selector? "#campaign-notification"
+      end
+    end
+
+    context "given view files are present for a green notification" do
+      setup do
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/campaign_green.erb")
+          .returns('<p>Green notification</p>')
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/campaign_red.erb")
+          .returns('')
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/campaign_black.erb")
+          .returns('')
+      end
+
+      should "show a green campaign notification on the page" do
+        visit "/templates/campaign.html.erb"
+        assert page.has_selector? "#campaign-notification.green"
+        assert_match '<p>Green notification</p>', page.body
+      end
+    end
+
+    context "given view files are present for a red notification" do
+      setup do
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/campaign_green.erb")
+          .returns('')
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/campaign_red.erb")
+          .returns('<p>Red campaign</p>')
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/campaign_black.erb")
+          .returns('')
+      end
+
+      should "show a red notification on the page" do
+        visit "/templates/campaign.html.erb"
+        assert page.has_selector? "#campaign-notification.red"
+        assert_match '<p>Red campaign</p>', page.body
+      end
+    end
+
+    context "given view files are present for a black notification" do
+      setup do
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/campaign_green.erb")
+          .returns('')
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/campaign_red.erb")
+          .returns('')
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/campaign_black.erb")
+          .returns('<p>Black campaign</p>')
+      end
+
+      should "show a black notification on the page" do
+        visit "/templates/campaign.html.erb"
+        assert page.has_selector? "#campaign-notification.black"
+        assert_match '<p>Black campaign</p>', page.body
       end
     end
   end
