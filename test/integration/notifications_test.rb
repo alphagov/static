533c660532de629646523fe30e0000a273b4545f
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2012-10-11 14:01:12 +0000

    Namespace banner integration tests

--- test/integration/notifications_test.rb
+++ test/integration/notifications_test.rb
@@ -5,51 +5,53 @@ class NotificationsTest < ActionDispatch::IntegrationTest
     NotificationFileLookup.banner_file = nil
 	end
 
-  context "given view files are empty" do
-  	setup do
-      File.stubs(:read).with("#{Rails.root}/app/views/notifications/banner_green.erb")
-				.returns('')
-			File.stubs(:read).with("#{Rails.root}/app/views/notifications/banner_red.erb")
-				.returns('')
-  	end
+  context "banner notifications" do
+    context "given view files are empty" do
+      setup do
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/banner_green.erb")
+          .returns('')
+        File.stubs(:read).with("#{Rails.root}/app/views/notifications/banner_red.erb")
+          .returns('')
+      end
 
-  	should "should not show a banner notification on the page" do
-	    visit "/templates/wrapper.html.erb"
-	    refute page.has_selector? "#banner-notification"
-	  end
-	end
-
-	context "given view files are present for a green notification" do
-		setup do
-			File.stubs(:read).with("#{Rails.root}/app/views/notifications/banner_green.erb")
-				.returns('<p>Everything is fine</p>')
-      File.stubs(:read).with("#{Rails.root}/app/views/notifications/banner_red.erb")
-				.returns('')
-		end
+      should "should not show a banner notification on the page" do
+        visit "/templates/wrapper.html.erb"
+        refute page.has_selector? "#banner-notification"
+      end
+    end
 
     context "given view files are present for a green notification" do
       setup do
         File.stubs(:read).with("#{Rails.root}/app/views/notifications/banner_green.erb")
           .returns('<p>Everything is fine</p>')
-      end
-
-      should "show a banner notification on the page" do
-        visit "/templates/wrapper.html.erb"
-        assert page.has_selector? "#banner-notification.green"
-        assert_match '<p>Everything is fine</p>', page.body
-      end
-    end
-
-    context "given view files are present for a red notification" do
-      setup do
         File.stubs(:read).with("#{Rails.root}/app/views/notifications/banner_red.erb")
-          .returns('<p>Everything is fine</p>')
+          .returns('')
       end
 
-      should "show a banner notification on the page" do
-        visit "/templates/wrapper.html.erb"
-        assert page.has_selector? "#banner-notification.red"
-        assert_match '<p>Everything is fine</p>', page.body
+      context "given view files are present for a green notification" do
+        setup do
+          File.stubs(:read).with("#{Rails.root}/app/views/notifications/banner_green.erb")
+            .returns('<p>Everything is fine</p>')
+        end
+
+        should "show a banner notification on the page" do
+          visit "/templates/wrapper.html.erb"
+          assert page.has_selector? "#banner-notification.green"
+          assert_match '<p>Everything is fine</p>', page.body
+        end
+      end
+
+      context "given view files are present for a red notification" do
+        setup do
+          File.stubs(:read).with("#{Rails.root}/app/views/notifications/banner_red.erb")
+            .returns('<p>Everything is fine</p>')
+        end
+
+        should "show a banner notification on the page" do
+          visit "/templates/wrapper.html.erb"
+          assert page.has_selector? "#banner-notification.red"
+          assert_match '<p>Everything is fine</p>', page.body
+        end
       end
     end
   end
