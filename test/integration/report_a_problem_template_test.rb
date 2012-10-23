6f80aca4e31203dd16aed58c748a999602501f8e
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-10-23 09:14:13 +0000

    Add test for html escaping in report_a_problem template.

---
+++ test/integration/report_a_problem_template_test.rb
@@ -0,0 +1,20 @@
+require_relative "../test_helper"
+
+class ReportAProblemTemplateTest < ActionDispatch::IntegrationTest
+  include ERB::Util
+
+  def get_template
+    get "/templates/report_a_problem.raw.html.erb"
+    response.body
+  end
+
+  should "sanitise the return URL in the template" do
+    # The template is rendered in Rack middleware, and therefore doesn't benefit from Rails' auto escaping
+    template = get_template
+
+    request_url = "<foo&bar>"
+    result = ERB.new(template).result(binding)
+
+    assert result =~ /&lt;foo&amp;bar&gt;/
+  end
+end
