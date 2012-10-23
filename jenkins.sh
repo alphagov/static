633d2d6800507697f8f43e25903872a2ce578fd4
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-10-23 09:22:53 +0000

    Assets precompile doesn't want to run under RAILS_ENV=test

--- jenkins.sh
+++ jenkins.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-export RAILS_ENV=test
-
 git clean -fdx
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 
-bundle exec rake
+RAILS_ENV=test bundle exec rake
 bundle exec rake assets:precompile
