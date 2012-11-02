8d165d68339d3f2815ccb8e92eb2f4c47268f625
tombye <tom.byers@digital.cabinet-office.gov.uk>
2012-11-02 16:11:20 +0000

    Fix for https://govuk.zendesk.com/tickets/9662
    Other relevant links box overlaps cookie message as it is not considered
    when the space it can move in is calculated.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -159,8 +159,18 @@ function getCookie(name){
 }
 
 $(function() {
-  if (document.getElementById('global-cookie-message') && getCookie('seen_cookie_message') === null) {
-    $('#global-cookie-message').show();
+  var $message = $('#global-cookie-message'),
+      $relatedColumn = $('#wrapper .related-positioning');
+
+  if ($message.length && getCookie('seen_cookie_message') === null) {
+    if ($relatedColumn.length) {
+      // related content box needs to know the top position of the footer
+      // this changes when content is split into tabs
+      if (typeof GOVUK.stopScrollingAtFooter !== 'undefined') {
+              GOVUK.stopScrollingAtFooter.updateFooterTop();
+      }
+    }
+    $message.show();
     setCookie('seen_cookie_message', 'yes', 28);
   }
 });
