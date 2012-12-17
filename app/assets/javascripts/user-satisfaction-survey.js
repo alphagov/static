69772ea206b50e17fd925542175f2b731aff31df
Kushal Pisavadia <kushi.p@gmail.com>
2012-12-17 17:27:52 +0000

    Initial skeleton for UserSatisfaction object
    This object will be used to manage showing the user satisfaction survey
    banner call-to-action to users.
    
    Currently it just sets the relevant cookie key/value so that we can
    track when users have seen the cookie bar and when they've taken the
    survey.

---
+++ app/assets/javascripts/user-satisfaction-survey.js
@@ -0,0 +1,11 @@
+var GOVUK = GOVUK || {};
+GOVUK.UserSatisfaction = GOVUK.UserSatisfaction || function () {
+};
+
+GOVUK.UserSatisfaction.prototype = {
+  cookieNameSeenBar: "seenSurveyBar",
+  cookieNameTakenSurvey: "takenUserSatisfactionSurvey",
+
+  setCookieSeenBar: function () { return Alphagov.write_cookie(this.cookieNameSeenBar, true); },
+  setCookieTakenSurvey: function () { return Alphagov.write_cookie(this.cookieNameTakenSurvey, true); },
+};
