2530fa4132653a838ba54089f0a6d4bae68979e9
Kushal Pisavadia <kushi.p@gmail.com>
2013-01-03 12:10:20 +0000

    Add click event handlers to set cookies
    When a user clicks one of the links we should set a cookie to
    acknowledge that they shouldn't see the satisfaction survey ever again.

--- spec/javascripts/UserSatisfactionSpec.js
+++ spec/javascripts/UserSatisfactionSpec.js
@@ -5,10 +5,37 @@ describe("User Satisfaction Survery", function () {
     var cookie = new GOVUK.Cookie();
     var surveyElement;
 
+    var clickElem = function (link) {
+      var cancelled = false;
+
+      if (document.createEvent) {
+        var event = document.createEvent("MouseEvents");
+        event.initMouseEvent("click", true, true, window,
+                             0, 0, 0, 0, 0,
+                             false, false, false, false,
+                             0, null);
+        cancelled = !link.dispatchEvent(event);
+      } else if (link.fireEvent) {
+        cancelled = !link.fireEvent("onclick");
+      }
+
+      if (!cancelled) {
+        window.location = link.href;
+      }
+    }
+
+    var block = '<section id="user-satisfaction-survey">\
+      <div class="wrapper">\
+        <h1>Tell us what you think of GOV.UK</h1>\
+        <p>Survey takes 5 minutes and opens in a new window <a href="javascript:void(0)" id="survey-no-thanks">No thanks</a></p>\
+        <p class="right"><a href="javascript:void(0)" id="take-survey" class="button">5 min survey</a></p>\
+      </div>\
+    </section>';
+
     beforeEach(function () {
-      surveyElement = document.createElement("div");
-      document.body.appendChild(surveyElement);
-      surveyElement.id = "user-satisfaction-survey";
+      document.body.insertAdjacentHTML("afterbegin", block);
+
+      surveyElement = document.getElementById("user-satisfaction-survey");
       surveyElement.style.display = "none";
 
       survey = new GOVUK.UserSatisfaction();
@@ -71,6 +98,26 @@ describe("User Satisfaction Survery", function () {
       }
 
       expect(counter).toBe(0);
+    });
+
+    describe("Event handlers", function () {
+      it("should set a cookie when clicking 'take survey'", function () {
+        survey.showSurveyBar();
+
+        var takeSurvey = document.getElementById("take-survey");
+        clickElem(takeSurvey);
+
+        expect(cookie.read(survey.cookieNameTakenSurvey)).toBe('true');
+      });
+
+      it("should set a cookie when clicking 'no thanks'", function () {
+        survey.showSurveyBar();
+
+        var noThanks = document.getElementById("survey-no-thanks");
+        clickElem(noThanks);
+
+        expect(cookie.read(survey.cookieNameTakenSurvey)).toBe('true');
+      });
     });
   });
 });
