f94b5df2854e253c548eca7ea3236790eb10ef3b
Alex Torrance <hello@alextorrance.co.uk>
2013-01-10 14:55:50 +0000

    Remove design principles pop up javascript

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,74 +1,3 @@
-$(function() {
-  function getUrlVars() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0, len = hashes.length; i < len; i++) {
-      hash = hashes[i].split('=');
-      vars.push(hash[0]);
-      vars[hash[0]] = hash[1];
-    }
-    return vars;
-  }
-
-  function getUrlVar(name){
-    return getUrlVars()[name];
-  }
-
-  function onDesignPrinciples() {
-    var pathName = window.location.pathname,
-        isDP = !! pathName.match(/^\/designprinciples/),
-        isPF;
-    
-    if (isDP) {
-      // performance framework is in beta so return false to force that popup
-      isPF = !! pathName.match(/^\/designprinciples\/performanceframework/);
-      if (isPF) {
-        return false;
-      }
-      
-      return true;
-    }
-    
-    return false;
-  }
-
-  function activeCookieName() {
-    if (onDesignPrinciples()){
-      return 'dp-tour';
-    } else {
-      return 'govuk-tour';
-    }
-  }
-
-
-
-
-  
-
-  $(".welcome-tour .close").live("click", function(){
-    setCookie(activeCookieName(), "dismiss", 7);
-    return;
-  });
-
-  $(".thanks-dismiss").live("click", function(){
-    setCookie(activeCookieName(), "dismiss", 7);
-    closePopup();
-    return false;
-  });
-
-  closePopup = function(){
-    $("#popup").fadeOut(400, function(){
-      $("#mask").slideUp('fast');
-      $("#mask").remove();
-      $("#popup").remove();
-    });
-
-    $(".header-global h1 a").focus();
-  };
-
-
-});
-
 function setCookie(name, value, days){
   var cookieString = name + "=" + value + "; path=/";
   if (days) {
@@ -99,8 +28,8 @@ function getCookie(name){
 
 $(function() {
   var addStyle,
-      $message = $('#global-cookie-message'),
-      $relatedColumn = $('#wrapper .related-positioning');
+  $message = $('#global-cookie-message'),
+  $relatedColumn = $('#wrapper .related-positioning');
 
   if ($message.length && getCookie('seen_cookie_message') === null) {
     if ($relatedColumn.length) {
@@ -109,7 +38,7 @@ $(function() {
       // related content box needs to know the top position of the footer
       // this changes when content is split into tabs
       if (typeof GOVUK.stopScrollingAtFooter !== 'undefined') {
-              GOVUK.stopScrollingAtFooter.updateFooterTop();
+        GOVUK.stopScrollingAtFooter.updateFooterTop();
       }
     }
     $message.show();
