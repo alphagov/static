348210625d299ef37b84e7b928111480a877a902
Frances Berriman <fberriman@gmail.com>
2013-03-11 12:06:44 +0000

    Adding a couple comments

--- app/assets/javascripts/browser-check.js
+++ app/assets/javascripts/browser-check.js
@@ -1,18 +1,21 @@
 $(function() {
 	var $prompt = $("#global-browser-prompt");
 
+	// we don't show the message when the cookie warning is also there
 	if($("#global-cookie-warning").length == 0){
 		if ( suchi.isOld(navigator.userAgent) ) {
 			if(getCookie('not_first_visit') == "yes" && getCookie('browser_upgrade_dismissed') == null){
 				$prompt.show();
 				$prompt.find(".dismiss").on("click", function(){
 					$prompt.hide();
+					// the warning is dismissable for 2 weeks
 					setCookie('browser_upgrade_dismissed', 'yes', 14);
 					return false;
 				});
-			}
-		}
+			};
+		};
 	
+		// We're not showing the message on first visit
 		setCookie('not_first_visit', 'yes', 28);
-	}
+	};
 });
