398d1719a639532482d9efda781c5933c34e04ef
Frances Berriman <fberriman@gmail.com>
2013-03-11 11:02:55 +0000

    Add cookies for dismissing and setting up the prompt. Also a dismiss link.

--- app/assets/javascripts/browser-check.js
+++ app/assets/javascripts/browser-check.js
@@ -1,5 +1,18 @@
-if ( suchi.isOld(navigator.userAgent) ) {
+$(function() {
+	var $prompt = $("#global-browser-prompt");
+
 	if($("#global-cookie-warning").length == 0){
-		$("#global-browser-prompt").show();
+		if ( suchi.isOld(navigator.userAgent) ) {
+			if(getCookie('not_first_visit') == "yes" && getCookie('browser_upgrade_dismissed') == null){
+				$prompt.show();
+				$prompt.find(".dismiss").on("click", function(){
+					$prompt.hide();
+					setCookie('browser_upgrade_dismissed', 'yes', 14);
+					return false;
+				});
+			}
+		}
+	
+		setCookie('not_first_visit', 'yes', 28);
 	}
-}
+});
