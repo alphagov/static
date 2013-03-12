9e87eb53d461a7039ca072f15dc01df7a7b469b2
Frances Berriman <fberriman@gmail.com>
2013-03-12 11:02:53 +0000

    Correct cookie names

--- app/assets/javascripts/browser-check.js
+++ app/assets/javascripts/browser-check.js
@@ -4,12 +4,12 @@ $(function() {
 	// we don't show the message when the cookie warning is also there
 	if($("#global-cookie-warning").length === 0){
 		if ( suchi.isOld(navigator.userAgent) ) {
-			if(getCookie('not_first_visit') !== null && getCookie('govuk_browser_upgrade_dismissed') === null){
+			if(getCookie('govuk_not_first_visit') !== null && getCookie('govuk_browser_upgrade_dismissed') === null){
 				$prompt.show();
 				$prompt.find(".dismiss").on("click", function(){
 					$prompt.hide();
 					// the warning is dismissable for 2 weeks
-					setCookie('browser_upgrade_dismissed', 'yes', 14);
+					setCookie('govuk_browser_upgrade_dismissed', 'yes', 14);
 					return false;
 				});
 			};
