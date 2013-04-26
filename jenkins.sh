93a0189bce71bd21bd38c1a21a5183a6d6cdba9a
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-04-26 13:57:31 +0000

    Export GOVUK_APP_DOMAIN to support Plek in routes file

--- jenkins.sh
+++ jenkins.sh
@@ -6,5 +6,7 @@ git clean -fdx
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 
+export GOVUK_APP_DOMAIN=dev.gov.uk
+
 RAILS_ENV=test bundle exec rake
 bundle exec rake assets:precompile
