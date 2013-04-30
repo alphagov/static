ab19e45e1a2fa9cc58c5ebd6b8f9f95106792d17
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-04-30 16:03:15 +0000

    Re-organise routes to group templates by type

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Static::Application.routes.draw do
-  get "/templates/campaign.html.erb", :to => "root#campaign"
+
+  # Templates
   get "/templates/wrapper.html.erb", :to => "root#wrapper"
   get "/templates/wrapper_with_js_last.html.erb", :to => "root#wrapper_with_js_last"
   get "/templates/print.html.erb", :to => "root#print"
-  get "/templates/related.raw.html.erb", :to => "root#related"
-  get "/templates/report_a_problem.raw.html.erb", :to => "root#report_a_problem"
   get "/templates/homepage.html.erb", :to => "root#homepage"
   get "/templates/admin.html.erb", :to => "root#admin"
+  get "/templates/header_footer_only.html.erb", :to => "root#header_footer_only"
+  get "/templates/chromeless.html.erb", :to => "root#chromeless"
+
+  get "/templates/barclays_epdq.html", :to => "root#barclays_epdq"
+
+  # Snippets
+  get "/templates/campaign.html.erb", :to => "root#campaign"
+  get "/templates/related.raw.html.erb", :to => "root#related"
+  get "/templates/report_a_problem.raw.html.erb", :to => "root#report_a_problem"
+  get "/templates/beta_notice.html.erb", :to => "root#beta_notice"
+  get "/templates/proposition_menu.html.erb", :to => "root#proposition_menu"
+
+  # Errors
   get "/templates/404.html.erb", :to => "root#404"
   get "/templates/406.html.erb", :to => "root#406"
   get "/templates/410.html.erb", :to => "root#410"
@@ -15,13 +27,8 @@ Static::Application.routes.draw do
   get "/templates/501.html.erb", :to => "root#501"
   get "/templates/503.html.erb", :to => "root#503"
   get "/templates/504.html.erb", :to => "root#504"
-  get "/templates/header_footer_only.html.erb", :to => "root#header_footer_only"
-  get "/templates/chromeless.html.erb", :to => "root#chromeless"
-  get "/templates/beta_notice.html.erb", :to => "root#beta_notice"
-  get "/templates/proposition_menu.html.erb", :to => "root#proposition_menu"
 
-  get "/templates/barclays_epdq.html", :to => "root#barclays_epdq"
-
+  # Icon redirects
   get "/apple-touch-icon.png", :to => "apple_icons#show"
   get "/apple-touch-icon-144x144.png", :to => "apple_icons#show"
   get "/apple-touch-icon-114x114.png", :to => "apple_icons#show"
