4092c7dbd94c72046a852c1c17c316018442f53f
Jake Benilov <benilov@gmail.com>
2013-05-17 13:02:54 +0100

    bugfix: report a problem jquery selectors

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -134,7 +134,7 @@ $(document).ready(function() {
   });
 
   // form submission for reporting a problem
-  var $forms = $('.report-a-problem-container form', '.report-a-problem form');
+  var $forms = $('.report-a-problem-container form, .report-a-problem form');
   $forms.append('<input type="hidden" name="javascript_enabled" value="true"/>');
   $forms.append($('<input type="hidden" name="referrer">').val(document.referrer || "unknown"));
 
