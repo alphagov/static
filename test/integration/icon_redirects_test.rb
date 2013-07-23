ad3b33778e940e145a3db3538dc0ee8d140ff0c3
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2013-07-23 13:08:52 +0000

    Fix redirects for apple-touch icons
    We were redirecting to non-existent assets for /apple-touch-icon.png and
    /apple-touch-icon-precomposed.png.  This started to fail noisily since
    alphagov/static#289 was merged.
    
    Add an override to redirect them to an asset that does exist. Also added
    tests to ensure that the targets exist.

--- test/integration/icon_redirects_test.rb
+++ test/integration/icon_redirects_test.rb
@@ -4,18 +4,34 @@ class IconRedirectsTest < ActionDispatch::IntegrationTest
 
   [
     'favicon.ico',
-    'apple-touch-icon.png',
     'apple-touch-icon-144x144.png',
     'apple-touch-icon-114x114.png',
     'apple-touch-icon-72x72.png',
     'apple-touch-icon-57x57.png',
-    'apple-touch-icon-precomposed.png',
   ].each do |file|
     should "redirect #{file} to the asset path" do
       get "/#{file}"
       assert_equal 301, last_response.status
       # In development and test mode the asset pipeline doesn't add the hashes to the URLs
       assert_equal "http://example.org/static/#{file}", last_response.location
+    end
+
+    should "redirect #{file} to a location that exists" do
+      get "/static/#{file}"
+      assert_equal 200, last_response.status
+      assert last_response.body.size > 100
+    end
+  end
+
+  [
+    'apple-touch-icon.png',
+    'apple-touch-icon-precomposed.png',
+  ].each do |file|
+    should "redirect #{file} to the 57x57 icon" do
+      get "/#{file}"
+      assert_equal 301, last_response.status
+      # In development and test mode the asset pipeline doesn't add the hashes to the URLs
+      assert_equal "http://example.org/static/apple-touch-icon-57x57.png", last_response.location
     end
   end
 end
