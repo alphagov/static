8dfeeb2a50f7a9b36993dc7bb5f11f0863999bb7
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-08-30 14:43:55 +0100

    Browser JS style fixes
    - Spaces not tabs
    - Fix whitespace
    - Use 'on' properly to get clickable element
    - Obey JSLint

--- app/assets/javascripts/browser-check.js
+++ app/assets/javascripts/browser-check.js
@@ -1,22 +1,25 @@
-$(function() {
-	
-	// we don't show the message when the cookie warning is also there
-  if (getCookie('seen_cookie_message')) {
-		if ( suchi.isOld(navigator.userAgent) ) {
-			if(getCookie('govuk_not_first_visit') !== null && getCookie('govuk_browser_upgrade_dismissed') === null){
-				var $prompt = $("#global-browser-prompt");
-				$prompt.show();
-				$prompt.find(".dismiss").on("click", function(){
-					$prompt.hide();
-					// the warning is dismissable for 2 weeks
-					setCookie('govuk_browser_upgrade_dismissed', 'yes', 14);
-					return false;
-				});
-			}
-		}
-	
+/*globals $, getCookie, suchi, setCookie */
+/*jslint
+ white: true,
+ browser: true */
 
-		// We're not showing the message on first visit
-		setCookie('govuk_not_first_visit', 'yes', 28);
+$(function() {
+  "use strict";
+  // we don't show the message when the cookie warning is also there
+  if (getCookie('seen_cookie_message')) {
+    if (suchi.isOld(navigator.userAgent)) {
+      if(getCookie('govuk_not_first_visit') !== null && getCookie('govuk_browser_upgrade_dismissed') === null){
+        var $prompt = $("#global-browser-prompt");
+        $prompt.show();
+        $prompt.on("click", ".dismiss", function(e) {
+          $prompt.hide();
+          // the warning is dismissable for 2 weeks
+          setCookie('govuk_browser_upgrade_dismissed', 'yes', 14);
+        });
+      }
+    }
+
+    // We're not showing the message on first visit
+    setCookie('govuk_not_first_visit', 'yes', 28);
   }
 });
