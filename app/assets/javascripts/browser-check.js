39bdbc731bddc727ca9f1665026f3c5fd791b82e
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-08-30 15:02:29 +0100

    Use JS to show browser warning
    It doesn't really work (or erroneously works, since your browser might
    be good enough to avoid being shown the notice) when JS is unavailable,
    so it should only ever be shown using Javascript.

--- app/assets/javascripts/browser-check.js
+++ app/assets/javascripts/browser-check.js
@@ -5,11 +5,21 @@
 
 $(function() {
   "use strict";
+  function browserWarning() {
+    var container = $('<div id="global-browser-prompt"></div>'),
+        text = $('<p>For a safer, faster, better experience online you should upgrade your browser.</p>'),
+        findMoreLink = $('<a href="/support/browsers">Find out more about browsers</a>'),
+        closeLink = $('<a href="#" class="dismiss" title="Dismiss this message">Close</a>');
+
+    return container.append(text.append(findMoreLink, closeLink));
+  }
+
   // we don't show the message when the cookie warning is also there
   if (getCookie('seen_cookie_message')) {
     if (suchi.isOld(navigator.userAgent)) {
       if(getCookie('govuk_not_first_visit') !== null && getCookie('govuk_browser_upgrade_dismissed') === null){
-        var $prompt = $("#global-browser-prompt");
+        var $prompt = browserWarning();
+        $('#global-cookie-message').after($prompt);
         $prompt.show();
         $prompt.on("click", ".dismiss", function(e) {
           $prompt.hide();
