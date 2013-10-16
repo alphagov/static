201ae3ffa788dee2df707d1386982bf91ed9b879
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2013-10-16 12:24:26 +0100

    refactor to use trycatcher (for now) and move shim to lib

--- app/assets/javascripts/analytics/print-intent.js
+++ app/assets/javascripts/analytics/print-intent.js
@@ -1,28 +1,32 @@
-// Extension to monitor attempts to print pages.
-try {
-    (function() {
-        "use strict";
-
-        var printAttempt = function() {
-            /** TODO:
-             * Should this be
-               * GOVUK.sendToAnalytics()
-               * _gaq / ga
-               * or somthing else?
-             */
-            _gaq.push(['_trackEvent', 'Print Intent', document.location.pathname]); //for classic GA
-            // ga('send', 'event', 'Print Intent', document.location.pathname); //for Universal GA
-        };
-        // We should have window.matchMedia available via javascripts/match-media.js shim
-        if (window.matchMedia) {
-            var mediaQueryList = window.matchMedia('print'),
-                mqlListenerCount = 0;
-            mediaQueryList.addListener(function(mql) {
-                if (!mql.matches && mqlListenerCount < 1) {
-                    printAttempt();
-                }
-            });
+function tryCatcher( fn, message ){
+    return function(){
+        try {
+            fn.apply(this, arguments);
+        } catch (e) {
+            console.log( message, e );
         }
-        window.onafterprint = printAttempt;
-    }());
-} catch (e) {}
+    };
+}
+
+// Extension to monitor attempts to print pages.
+(tryCatcher(function() {
+
+    "use strict";
+
+    var printAttempt = tryCatcher(function() {
+        _gaq.push(['_trackEvent', 'Print Intent', document.location.pathname]); //for classic GA
+        // ga('send', 'event', 'Print Intent', document.location.pathname); //for Universal GA
+    },'gaq event push failed');
+
+    // We should have window.matchMedia available via javascripts/match-media.js shim
+    if (window.matchMedia) {
+        var mediaQueryList = window.matchMedia('print'),
+            mqlListenerCount = 0;
+        mediaQueryList.addListener(tryCatcher(function(mql) {
+            if (!mql.matches && mqlListenerCount < 1) {
+                printAttempt();
+            }
+        },'no matchMedia matching available'));
+    }
+    window.onafterprint = printAttempt;
+},'Print attempt failed')());
