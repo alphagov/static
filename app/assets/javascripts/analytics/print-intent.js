7edadc4dfaa64c85e86c206a36a4e7ba6e750337
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2013-10-17 11:37:37 +0100

    ensure we do not send multiple events in chrome

--- app/assets/javascripts/analytics/print-intent.js
+++ app/assets/javascripts/analytics/print-intent.js
@@ -12,7 +12,6 @@ function tryCatcher( fn, message ){
 (tryCatcher(function() {
 
     "use strict";
-
     var printAttempt = tryCatcher(function() {
         _gaq.push(['_trackEvent', 'Print Intent', document.location.pathname]); //for classic GA
         // ga('send', 'event', 'Print Intent', document.location.pathname); //for Universal GA
@@ -23,10 +22,19 @@ function tryCatcher( fn, message ){
         var mediaQueryList = window.matchMedia('print'),
             mqlListenerCount = 0;
         mediaQueryList.addListener(tryCatcher(function(mql) {
-            if (!mql.matches && mqlListenerCount < 1) {
+            if (!mql.matches && mqlListenerCount === 0) {
                 printAttempt();
+                mqlListenerCount++;
+                // If we try and print again in 3 seconds, don't log it
+                window.setTimeout(function(){
+                    mqlListenerCount = 0;
+                },1e3);
             }
         },'no matchMedia matching available'));
     }
-    window.onafterprint = printAttempt;
+
+    if(window.onafterprint){
+        window.onafterprint = printAttempt;
+    }
+
 },'Print attempt failed')());
