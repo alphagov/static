b36f7a7d2dd6596235d3eb0d866dbc1e5177469b
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-12-20 13:14:51 +0000

    Remove campaign notifications from static
    Since https://github.com/alphagov/frontend/pull/490 was merged these
    aren't needed here as the homepage functionality is completely in the
    frontend application.

--- test/unit/notification_helper_test.rb
+++ test/unit/notification_helper_test.rb
@@ -11,64 +11,25 @@ describe "Notification Helper" do
 
     describe "when a banner is present" do
       it "should return a banner notification" do
-        campaign = {:file => "<p>You've got notifications!</p>", :colour => :green}
-        NotificationFileLookup.any_instance.stubs(:banner).returns(campaign)
+        banner = {:file => "<p>You've got notifications!</p>", :colour => :green}
+        NotificationFileLookup.any_instance.stubs(:banner).returns(banner)
         assert_match "<p>You've got notifications!</p>", banner_notification
       end
 
       it "should have a section wrapper with the banner colour for a green notification" do
-        campaign = {:file => "<p>You've got notifications!</p>", :colour => :green}
-        NotificationFileLookup.any_instance.stubs(:banner).returns(campaign)
+        banner = {:file => "<p>You've got notifications!</p>", :colour => :green}
+        NotificationFileLookup.any_instance.stubs(:banner).returns(banner)
 
         assert_equal "<section class=\"green\" id=\"banner-notification\"><div><p>You've got notifications!</p></div></section>",
                      banner_notification
       end
 
       it "should have a section wrapper with the banner colour for a red notification" do
-        campaign = {:file => "<p>You've got notifications!</p>", :colour => :red}
-        NotificationFileLookup.any_instance.stubs(:banner).returns(campaign)
+        banner = {:file => "<p>You've got notifications!</p>", :colour => :red}
+        NotificationFileLookup.any_instance.stubs(:banner).returns(banner)
 
         assert_equal "<section class=\"red\" id=\"banner-notification\"><div><p>You've got notifications!</p></div></section>",
                      banner_notification
-      end
-    end
-  end
-
-  describe "campaign_replacement_notification" do
-    it "should return an empty string if no campaign is present" do
-      NotificationFileLookup.any_instance.stubs(:campaign).returns(nil)
-      assert_equal "", campaign_replacement_notification
-    end
-
-    describe "when a campaign is present" do
-      it "should return a campaign notification" do
-        campaign = {:file => "<p>You've got notifications!</p>", :colour => :green}
-        NotificationFileLookup.any_instance.stubs(:campaign).returns(campaign)
-        assert_match "<p>You've got notifications!</p>", campaign_replacement_notification
-      end
-
-      it "should have a section wrapper with the campaign colour for a green notification" do
-        campaign = {:file => "<p>You've got notifications!</p>", :colour => :green}
-        NotificationFileLookup.any_instance.stubs(:campaign).returns(campaign)
-
-        assert_equal "<section class=\"green\" id=\"campaign-notification\"><div><p>You've got notifications!</p></div></section>",
-                     campaign_replacement_notification
-      end
-
-      it "should have a section wrapper with the campaign colour for a red notification" do
-        campaign = {:file => "<p>You've got notifications!</p>", :colour => :red}
-        NotificationFileLookup.any_instance.stubs(:campaign).returns(campaign)
-
-        assert_equal "<section class=\"red\" id=\"campaign-notification\"><div><p>You've got notifications!</p></div></section>",
-                     campaign_replacement_notification
-      end
-
-      it "should have a section wrapper with the campaign colour for a black notification" do
-        campaign = {:file => "<p>You've got notifications!</p>", :colour => :black}
-        NotificationFileLookup.any_instance.stubs(:campaign).returns(campaign)
-
-        assert_equal "<section class=\"black\" id=\"campaign-notification\"><div><p>You've got notifications!</p></div></section>",
-                     campaign_replacement_notification
       end
     end
   end
