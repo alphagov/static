de5584642dd7e2186c22d574eef6d677c5aedf0f
Edd Sowden <e@e26.co.uk>
2014-09-09 13:06:25 +0000

    Update the icon redirects to new sizes
    The new sizes are being hit far more in production (by more than 16x) than the
    old ones. The old ones also no longer exist in the new version of the
    govuk_template..

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Static::Application.routes.draw do
 
   # Icon redirects
   get "/favicon.ico", :to => "icon_redirects#show"
-  get "/apple-touch-icon.png", :to => "icon_redirects#apple_57_57"
-  get "/apple-touch-icon-144x144.png", :to => "icon_redirects#show"
-  get "/apple-touch-icon-114x114.png", :to => "icon_redirects#show"
-  get "/apple-touch-icon-72x72.png", :to => "icon_redirects#show"
-  get "/apple-touch-icon-57x57.png", :to => "icon_redirects#show"
-  get "/apple-touch-icon-precomposed.png", :to => "icon_redirects#apple_57_57"
+  get "/apple-touch-icon.png", :to => "icon_redirects#apple_60_60"
+  get "/apple-touch-icon-152x152.png", :to => "icon_redirects#show"
+  get "/apple-touch-icon-120x120.png", :to => "icon_redirects#show"
+  get "/apple-touch-icon-76x76.png", :to => "icon_redirects#show"
+  get "/apple-touch-icon-60x60.png", :to => "icon_redirects#show"
+  get "/apple-touch-icon-precomposed.png", :to => "icon_redirects#apple_60_60"
 end
--- test/integration/icon_redirects_test.rb
+++ test/integration/icon_redirects_test.rb
@@ -4,10 +4,10 @@ class IconRedirectsTest < ActionDispatch::IntegrationTest
 
   [
     'favicon.ico',
-    'apple-touch-icon-144x144.png',
-    'apple-touch-icon-114x114.png',
-    'apple-touch-icon-72x72.png',
-    'apple-touch-icon-57x57.png',
+    'apple-touch-icon-152x152.png',
+    'apple-touch-icon-120x120.png',
+    'apple-touch-icon-76x76.png',
+    'apple-touch-icon-60x60.png',
   ].each do |file|
     should "redirect #{file} to the asset path" do
       get "/#{file}"
@@ -33,11 +33,11 @@ class IconRedirectsTest < ActionDispatch::IntegrationTest
     'apple-touch-icon.png',
     'apple-touch-icon-precomposed.png',
   ].each do |file|
-    should "redirect #{file} to the 57x57 icon" do
+    should "redirect #{file} to the 60x60 icon" do
       get "/#{file}"
       assert_equal 301, last_response.status
       # In development and test mode the asset pipeline doesn't add the hashes to the URLs
-      assert_equal "http://example.org/static/apple-touch-icon-57x57.png", last_response.location
+      assert_equal "http://example.org/static/apple-touch-icon-60x60.png", last_response.location
     end
   end
 end
