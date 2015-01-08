1f3d1e39f5b2d7540354e6504a14b52ddfa215c1
Ralph Cowling <ralph.cowling@digital.cabinet-office.gov.uk>
2015-01-08 15:00:43 +0000

    Single Indent

--- app/assets/javascripts/analytics/error-tracking.js
+++ app/assets/javascripts/analytics/error-tracking.js
@@ -1,25 +1,25 @@
 // Extension to track errors using google analytics as a data store.
 (function() {
 
-    "use strict";
-    var trackJavaScriptError = function (e) {
-      var errorSource = e.filename + ': ' + e.lineno;
-      _gaq.push([
-        '_trackEvent',
-        'JavaScript Error',
-        e.message,
-        errorSource,
-        1, // Set our value to 1, though we could look to tally session errors here
-        true // nonInteractive so bounce rate isn't affected
-      ]);
-    };
+  "use strict";
+  var trackJavaScriptError = function (e) {
+    var errorSource = e.filename + ': ' + e.lineno;
+    _gaq.push([
+      '_trackEvent',
+      'JavaScript Error',
+      e.message,
+      errorSource,
+      1, // Set our value to 1, though we could look to tally session errors here
+      true // nonInteractive so bounce rate isn't affected
+    ]);
+  };
 
-    if (window.addEventListener) {
-      window.addEventListener('error', trackJavaScriptError, false);
-    } else if (window.attachEvent) {
-      window.attachEvent('onerror', trackJavaScriptError);
-    } else {
-      window.onerror = trackJavaScriptError;
-    }
+  if (window.addEventListener) {
+    window.addEventListener('error', trackJavaScriptError, false);
+  } else if (window.attachEvent) {
+    window.attachEvent('onerror', trackJavaScriptError);
+  } else {
+    window.onerror = trackJavaScriptError;
+  }
 
 }());
