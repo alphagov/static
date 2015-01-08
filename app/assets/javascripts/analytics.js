660cf851ff92888b17407ad075469de0528a7c74
Edd Sowden <e@e26.co.uk>
2015-01-08 10:12:03 +0000

    Remove old analytics JavaScript
    This analytics was added for the performance platform to create an
    engagement score of pages. They have since stopped using these events
    and they are no longer needed. Remove them so they stop sending lots of
    events to Google Analytics.
    
    Tidy up the function in `tracking.js` which is still needed so that it
    is clearer as to what is going on.

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,5 +1,3 @@
-//= require analytics/events-cookie-handler
-//= require analytics/tracking-strategies
 //= require analytics/tracking
 //= require analytics/print-tracking
 //= require analytics/print-intent
