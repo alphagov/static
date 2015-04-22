57202c332eee99a337bfdacef6f9e575b0a1d654
Paul Hayes <fofr@users.noreply.github.com>
2015-04-22 13:30:11 +0100

    Cleanup addToggleLink method
    * Don’t concatenate string, use `\` line break
    * Move `var` declaration to top
    * Keep quotes consistent

--- app/assets/javascripts/report-a-problem.js
+++ app/assets/javascripts/report-a-problem.js
@@ -4,22 +4,21 @@
 
   var ReportAProblem = function ($container) {
     this.$container = $container;
-
     this.form = new GOVUK.ReportAProblemForm($container.find('form'));
-
     this.addToggleLink();
   };
 
   ReportAProblem.prototype.addToggleLink = function() {
-    var $toggleBlock = $('<div class="report-a-problem-toggle-wrapper js-footer">' +
-                           '<p class="report-a-problem-toggle">' +
-                             '<a href="">Is there anything wrong with this page?</a>' +
-                           '</p>' +
-                         '</div>');
-    this.$container.before($toggleBlock);
+    var $container = this.$container,
+        $toggle = $('\
+      <div class="report-a-problem-toggle-wrapper js-footer">\
+        <p class="report-a-problem-toggle">\
+          <a href="">Is there anything wrong with this page?</a>\
+        </p>\
+      </div>');
 
-    var $container = this.$container;
-    $toggleBlock.on("click", '.report-a-problem-toggle a', function(evt) {
+    $container.before($toggle);
+    $toggle.on("click", ".report-a-problem-toggle a", function(evt) {
       $container.toggle();
       evt.preventDefault();
     });
