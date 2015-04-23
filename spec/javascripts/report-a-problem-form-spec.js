d394c4d7ab741bdac297e11e31eb3d2fd38f02ec
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-04-23 15:29:31 +0100

    Split report-a-problem spec
    Split the spec to test ReportAProblem and ReportAProblemForm seperately

---
+++ spec/javascripts/report-a-problem-form-spec.js
@@ -0,0 +1,66 @@
+describe("form submission for reporting a problem", function () {
+  var FORM_TEXT = '<form><button class="button" name="button" type="submit">Send</button></form>';
+  var $form, reportAProblemForm;
+
+  beforeEach(function() {
+    setFixtures(FORM_TEXT);
+    $form = $('form');
+    reportAProblemForm = new GOVUK.ReportAProblemForm($form);
+  });
+
+  it("should append a hidden 'javascript_enabled' field to the form", function() {
+    expect($form.find("[name=javascript_enabled]").val()).toBe("true");
+  });
+
+  it("should append a hidden 'referrer' field to the form", function() {
+    expect($form.find("[name=referrer]").val()).toBe("unknown");
+  });
+
+  describe("while the request is being handled", function() {
+    it("should disable the submit button to prevent multiple problem reports", function () {
+      spyOn($, "ajax").and.callFake(function(options) {});
+
+      $form.triggerHandler('submit');
+
+      expect($('.button')).toBeDisabled();
+    });
+  });
+
+  describe("if the request succeeds", function() {
+    it("should trigger a success event", function() {
+      spyOn($form, "trigger");
+      spyOn($, "ajax").and.callFake(function(options) {
+        options.success({message: 'great success!'});
+      });
+
+      $form.triggerHandler('submit');
+      expect($form.trigger).toHaveBeenCalledWith("reportAProblemForm.success", {message: 'great success!'});
+    });
+  });
+
+  describe("if the request is invalid", function() {
+    it("should re-enable the submit button, in order to allow the user to resubmit", function () {
+      spyOn($, "ajax").and.callFake(function(options) {
+        options.error({status: 422}, 'error');
+      });
+
+      $form.triggerHandler('submit');
+
+      expect($form).toBeVisible();
+      expect($('.button')).not.toBeDisabled();
+    });
+  });
+
+  describe("if the request has failed with a status 500", function() {
+    it("sshould trigger an error event", function() {
+      spyOn($form, "trigger");
+      spyOn($, "ajax").and.callFake(function(options) {
+        options.statusCode[500]({responseText: 'this might not even be JSON because nginx intercepts the error'});
+      });
+
+      $form.triggerHandler('submit');
+
+      expect($form.trigger).toHaveBeenCalledWith("reportAProblemForm.error");
+    });
+  });
+});
--- spec/javascripts/report-a-problem-spec.js
+++ spec/javascripts/report-a-problem-spec.js
@@ -12,14 +12,6 @@ describe("form submission for reporting a problem", function () {
         new GOVUK.ReportAProblem($('.report-a-problem-container'));
     });
 
-    it("should append a hidden 'javascript_enabled' field to the form", function() {
-        expect(form.find("[name=javascript_enabled]").val()).toBe("true");
-    });
-
-    it("should append a hidden 'referrer' field to the form", function() {
-        expect(form.find("[name=referrer]").val()).toBe("unknown");
-    });
-
     describe("clicking on the toggle", function(){
         it("should toggle the visibility of the form", function() {
             expect(form).toBeVisible();
@@ -34,49 +26,20 @@ describe("form submission for reporting a problem", function () {
         });
     });
 
-    describe("while the request is being handled", function() {
-        it("should disable the submit button to prevent multiple problem reports", function () {
-            spyOn($, "ajax").and.callFake(function(options) {});
-
-            form.triggerHandler('submit');
-
-            expect($('.button')).toBeDisabled();
-        });
-    });
-
     describe("if the request succeeds", function() {
         it("should replace the form with the response from the AJAX call", function() {
-            spyOn($, "ajax").and.callFake(function(options) {
-                options.success({message: 'great success!'});
-            });
 
-            form.triggerHandler('submit');
+            form.trigger('reportAProblemForm.success', {message: 'great success!'});
 
             expect(form).toBeHidden();
             expect($('.report-a-problem-content').html()).toEqual('great success!');
         });
     });
 
-    describe("if the request is invalid", function() {
-        it("should re-enable the submit button, in order to allow the user to resubmit", function () {
-            spyOn($, "ajax").and.callFake(function(options) {
-                options.error({status: 422}, 'error');
-            });
-
-            form.triggerHandler('submit');
-
-            expect(form).toBeVisible();
-            expect($('.button')).not.toBeDisabled();
-        });
-    });
-
-    describe("if the request has failed with a status 500", function() {
+    describe("if the request has failed", function() {
         it("should display an error message", function() {
-            spyOn($, "ajax").and.callFake(function(options) {
-                options.statusCode[500]({responseText: 'this might not even be JSON because nginx intercepts the error'});
-            });
 
-            form.triggerHandler('submit');
+            form.trigger('reportAProblemForm.error');
 
             expect(form).not.toBeVisible();
             expect($('.report-a-problem-content').html()).toContain("Sorry, we’re unable to receive your message");
