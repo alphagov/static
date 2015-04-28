db213648201ac9a2902f97901d2ecddfbac9429e
Jake Benilov <benilov@gmail.com>
2015-04-28 11:36:15 +0000

    Whitespace: 4 spaces to 2 spaces
    File was using 4 spaces for tabs instead of 2.

--- spec/javascripts/report-a-problem-spec.js
+++ spec/javascripts/report-a-problem-spec.js
@@ -1,48 +1,48 @@
 describe("form submission for reporting a problem", function () {
-    var FORM_TEXT = '<div class="report-a-problem-container">' +
-                      '<div class="report-a-problem-content">' +
-                        '<form><button class="button" name="button" type="submit">Send</button></form>' +
-                      '</div>' +
-                    '</div>';
-    var form;
+  var FORM_TEXT = '<div class="report-a-problem-container">' +
+                    '<div class="report-a-problem-content">' +
+                      '<form><button class="button" name="button" type="submit">Send</button></form>' +
+                    '</div>' +
+                  '</div>';
+  var form;
 
-    beforeEach(function() {
-        setFixtures(FORM_TEXT);
-        form = $('form');
-        new GOVUK.ReportAProblem($('.report-a-problem-container'));
+  beforeEach(function() {
+    setFixtures(FORM_TEXT);
+    form = $('form');
+    new GOVUK.ReportAProblem($('.report-a-problem-container'));
+  });
+
+  describe("clicking on the toggle", function(){
+    it("should toggle the visibility of the form", function() {
+      expect(form).toBeVisible();
+
+      $('a').click();
+
+      expect(form).toBeHidden();
+
+      $('a').click();
+
+      expect(form).toBeVisible();
     });
+  });
 
-    describe("clicking on the toggle", function(){
-        it("should toggle the visibility of the form", function() {
-            expect(form).toBeVisible();
+  describe("if the request succeeds", function() {
+    it("should replace the form with the response from the AJAX call", function() {
 
-            $('a').click();
+      form.trigger('reportAProblemForm.success', {message: 'great success!'});
 
-            expect(form).toBeHidden();
-
-            $('a').click();
-
-            expect(form).toBeVisible();
-        });
+      expect(form).toBeHidden();
+      expect($('.report-a-problem-content').html()).toEqual('great success!');
     });
+  });
 
-    describe("if the request succeeds", function() {
-        it("should replace the form with the response from the AJAX call", function() {
+  describe("if the request has failed", function() {
+    it("should display an error message", function() {
 
-            form.trigger('reportAProblemForm.success', {message: 'great success!'});
+      form.trigger('reportAProblemForm.error');
 
-            expect(form).toBeHidden();
-            expect($('.report-a-problem-content').html()).toEqual('great success!');
-        });
+      expect(form).not.toBeVisible();
+      expect($('.report-a-problem-content').html()).toContain("Sorry, we’re unable to receive your message");
     });
-
-    describe("if the request has failed", function() {
-        it("should display an error message", function() {
-
-            form.trigger('reportAProblemForm.error');
-
-            expect(form).not.toBeVisible();
-            expect($('.report-a-problem-content').html()).toContain("Sorry, we’re unable to receive your message");
-        });
-    });
+  });
 });
