5fb6323bdb2efd0547347114001014690d27e5a1
Jake Benilov <benilov@gmail.com>
2015-09-11 09:29:56 +0000

    Move running of Jasmine specs to the CI build
    Running the `spec:javascript` task within `rake test` seems to break
    the Rails integration tests (and I couldn't find a way around this,
    short of migrating to the `jasmine` gem).

--- jenkins.sh
+++ jenkins.sh
@@ -9,5 +9,6 @@ bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 export GOVUK_APP_DOMAIN=dev.gov.uk
 export GOVUK_WEBSITE_ROOT=http://www.dev.gov.uk
 
-RAILS_ENV=test bundle exec rake
+RAILS_ENV=test bundle exec rake test
+RAILS_ENV=test bundle exec rake spec:javascript
 bundle exec rake assets:precompile
