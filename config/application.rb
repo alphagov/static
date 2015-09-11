5ef6c2df61bbb979db130c2807c9dd82f68d9972
Jake Benilov <benilov@gmail.com>
2015-09-11 09:23:53 +0000

    Remove the filtering of param logs
    This app doesn't do anything with passwords.

--- config/application.rb
+++ config/application.rb
@@ -39,9 +39,6 @@ module Static
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
     config.assets.precompile += %w{
       favicon.ico
       *.js
