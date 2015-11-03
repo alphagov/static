34b45c1fd17421b4c41b7f6488e55bd7a7a64cd3
Robin Whittleton <robin.whittleton@digital.cabinet-office.gov.uk>
2015-11-03 10:00:51 +0000

    Add some initial functionality tests into Jasmine

---
+++ spec/javascripts/hmrc-webchat-spec.js
@@ -0,0 +1,49 @@
+describe('HMRC webchat', function () {
+  var INSERTION_HOOK = '<div class="heading-block"></div>';
+
+  beforeEach(function() {
+    spyOn(GOVUK.webchat, 'shouldOpen').and.returnValue(true);
+    setFixtures(INSERTION_HOOK);
+    GOVUK.webchat.init();
+  });
+
+  afterEach(function() {
+    $('.webchat-banner, body>iframe.hidden:last-child').remove();
+  });
+
+
+  it('should load a banner', function() {
+    expect($('.webchat-banner').length).toBe(1);
+  });
+
+  it('should load a hidden iframe', function() {
+    expect($('body>iframe.hidden:last-child').length).toBe(1);
+  });
+
+  describe('if it receives an open message', function(){
+
+    beforeEach(function(done){
+      window.postMessage(JSON.stringify({evt: 'opened'}), '*');
+      $(window).on('message',done);
+    });
+
+    it('should show the banner', function() {
+      expect($('.webchat-banner.open').length).toBe(1);
+    });
+
+  });
+
+  describe('if it receives a closed message', function(){
+
+    beforeEach(function(done){
+      window.postMessage(JSON.stringify({evt: 'closed'}), '*');
+      $(window).on('message',done);
+    });
+
+    it('should hide the banner', function() {
+      expect($('.webchat-banner:not(.open)').length).toBe(1);
+    });
+
+  });
+
+});
