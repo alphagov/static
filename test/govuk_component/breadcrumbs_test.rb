4790d0776463c42117084448a99ace1c72efed80
Paul Hayes <fofr@users.noreply.github.com>
2016-01-07 13:33:14 +0000

    Add breadcrumbs markup, styles and docs
    * Base styles on 20505dd7a398ea4ff7b449598049cff108dabab9
    * Remove `strong` styles as those don’t appear in the markup
    * Also based on #global-header in `_header.scss`

--- test/govuk_component/breadcrumbs_test.rb
+++ test/govuk_component/breadcrumbs_test.rb
@@ -11,4 +11,24 @@ class BreadcrumbsTestCase < ComponentTestCase
       assert_select ".govuk-breadcrumbs"
     end
   end
+
+  test "renders a single breadcrumb" do
+    render_component({ breadcrumbs: [{title: 'Section', url: '/section'}] })
+
+    assert_link_with_text_in('ol li:first-child', '/', 'Home')
+    assert_link_with_text_in('ol li:last-child', '/section', 'Section')
+  end
+
+  test "renders a list of breadcrumbs" do
+    render_component({
+      breadcrumbs: [
+        {title: 'Section', url: '/section'},
+        {title: 'Sub-section', url: '/sub-section'},
+      ]
+    })
+
+    assert_link_with_text_in('ol li:first-child', '/', 'Home')
+    assert_link_with_text_in('ol li:first-child + li', '/section', 'Section')
+    assert_link_with_text_in('ol li:last-child', '/sub-section', 'Sub-section')
+  end
 end
