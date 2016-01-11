9d7fccd5cd5f70633bda033e38c1553395d43f38
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2016-01-11 12:10:17 +0000

    Add related items markup, styles and docs
    * Based styles on 188e526 and core stylesheets https://github.com/alphagov/static/blob/master/app/assets/stylesheets/helpers/_core.scss#L56-L147

--- test/govuk_component/related_items_test.rb
+++ test/govuk_component/related_items_test.rb
@@ -11,4 +11,83 @@ class RelatedItemsTestCase < ComponentTestCase
       assert_select ".govuk-related-items"
     end
   end
+
+  test "renders a related items block" do
+    render_component({
+      sections: [
+        {
+          title: "Section title",
+          url: "/more-link",
+          items: [
+            {
+              title: "Item title",
+              url: "/item"
+            },
+            {
+              title: "Other item title",
+              url: "/other-item"
+            }
+          ]
+        }
+      ],
+    })
+
+    assert_select "h2", text: "Section title"
+    assert_link_with_text_in("ul li:last-child", "/more-link", "More")
+    assert_link_with_text_in("ul li:first-child", "/item", "Item title")
+    assert_link_with_text_in("ul li:first-child + li", "/other-item", "Other item title")
+  end
+
+  test "renders a multiple related items block" do
+    render_component({
+      sections: [
+        {
+          title: "Section title",
+          url: "/more-link",
+          items: [
+            {
+              title: "Item title",
+              url: "/item"
+            }
+          ]
+        },
+        {
+          title: "Other section title",
+          url: "/other-more-link",
+          items: [
+            {
+              title: "Other item title",
+              url: "/other-item"
+            }
+          ]
+        }
+      ],
+    })
+
+    assert_select "h2", text: "Section title"
+    assert_link_with_text_in("ul li:last-child", "/more-link", "More")
+    assert_link_with_text_in("ul li:first-child", "/item", "Item title")
+
+    assert_select "h2", text: "Other section title"
+    assert_link_with_text_in("ul li:last-child", "/other-more-link", "More")
+    assert_link_with_text_in("ul li:first-child", "/other-item", "Other item title")
+  end
+
+  test "has a default section title" do
+    render_component({
+      sections: [
+        {
+          url: "/more-link",
+          items: [
+            {
+              title: "Item title",
+              url: "/item"
+            }
+          ]
+        },
+      ],
+    })
+
+    assert_select "h2", text: "Elsewhere on GOV.UK"
+  end
 end
