ccd4e4e034b6ab4a50c1bc73d187dad7225589ad
Paul Hayes <fofr@users.noreply.github.com>
2016-03-02 14:57:02 +0000

    Add logo crests
    * Use the images provided by frontend toolkit
    * Make use of the retina versions
    * Port the crest mixin from _organisations.scss and add the retina
    versions, using the image dimensions to set background size correctly
    * Use `<span>` to push the text down so its baseline lines up with the
    coloured border

--- test/govuk_component/organisation_logo_test.rb
+++ test/govuk_component/organisation_logo_test.rb
@@ -11,4 +11,24 @@ class OrganisationLogoTestCase < ComponentTestCase
       assert_select ".govuk-organisation-logo"
     end
   end
+
+  test "organisation name appears" do
+    render_component(organisation: { name: "Oranisation name" })
+    assert_select ".govuk-organisation-logo .logo-container", text: "Oranisation name"
+  end
+
+  test "branding is added to the wrapping container" do
+    render_component(organisation: { name: "Branded", brand: "brand" })
+    assert_select ".govuk-organisation-logo.brand", text: "Branded"
+  end
+
+  test "a link is included when a URL is provided" do
+    render_component(organisation: { name: "Linked", url: "/somewhere" })
+    assert_select ".govuk-organisation-logo a.logo-container.logo-link[href='/somewhere']", text: "Linked"
+  end
+
+  test "a crest class is added when specified" do
+    render_component(organisation: { name: "Crested", crest: "single-identity" })
+    assert_select ".logo-container.logo-with-crest.crest-single-identity"
+  end
 end
