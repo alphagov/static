fc50ed656fd0429cd4de87c78e78ae3302eec5bf
Theodor Vararu <theodor.vararu@digital.cabinet-office.gov.uk>
2016-03-23 19:03:46 +0000

    Add RTL support to html publications.
    This fixes the positioning of the sticky element. The reason it's done
    on the parent element is because directly messing with the positioning
    of the element produces undesirable results.

--- test/govuk_component/govspeak_html_publication_test.rb
+++ test/govuk_component/govspeak_html_publication_test.rb
@@ -15,4 +15,14 @@ class GovspeakHtmlPublicationTestCase < GovspeakTestCase
       assert_select "[data-sticky-element] #my-content"
     end
   end
+
+  test "respects direction if passed in" do
+    assert_nothing_raised do
+      render_component(
+        content: '<span>Govspeak content</span>',
+        direction: 'rtl'
+      )
+      assert_select ".govuk-govspeak-html-publication.direction-rtl"
+    end
+  end
 end
