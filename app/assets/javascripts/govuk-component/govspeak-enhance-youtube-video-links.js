e2cd2b6a75f1a551a51eb554d2f3920b37fd7bb6
Gemma Leigh <hello@gemmaleigh.com>
2016-05-05 23:51:38 +0100

    Create a YoutubeVideoLinks module
    Start a YoutubeVideoLinks module, which parses the parseYoutubeVideoId,
    checks to see if it should convert a link within govspeak content to an
    embedded player and if so, converts YouTube links without the
    “data-youtube-player=‘off’” to be embedded media players.

--- app/assets/javascripts/govuk-component/govspeak-enhance-youtube-video-links.js
+++ app/assets/javascripts/govuk-component/govspeak-enhance-youtube-video-links.js
@@ -1,52 +1,66 @@
 //= require libs/jquery/jquery-ui-1.10.2.custom
 //= require vendor/jquery/jquery.player.min
 
-(function ($) {
-  window.GOVUK = window.GOVUK || {};
+(function(Modules) {
+  "use strict";
 
-  function parseYoutubeVideoId(string){
-    if(string.indexOf('youtube.com') > -1){
-      var i, _i, part, parts, params = {};
-      parts = string.split('?');
-      if (parts.length === 1){
-        return;
+  Modules.YoutubeVideoLinks = function() {
+
+    this.start = function($el) {
+
+      function parseYoutubeVideoId(string){
+        if(string.indexOf('youtube.com') > -1){
+          var i, _i, part, parts, params = {};
+          parts = string.split('?');
+          if (parts.length === 1){
+            return;
+          }
+          parts = parts[1].split('&');
+          for(i=0,_i=parts.length; i<_i; i++){
+            part = parts[i].split('=');
+            params[part[0]] = part[1];
+          }
+          return params.v;
+        }
+        if(string.indexOf('youtu.be') > -1){
+          var parts = string.split('/');
+          return parts.pop();
+        }
       }
-      parts = parts[1].split('&');
-      for(i=0,_i=parts.length; i<_i; i++){
-        part = parts[i].split('=');
-        params[part[0]] = part[1];
+
+      function shouldConvertToEmbeddedPlayer($link){
+        return $link.attr("data-youtube-player") != "off";
       }
-      return params.v;
-    }
-    if(string.indexOf('youtu.be') > -1){
-      var parts = string.split('/');
-      return parts.pop();
-    }
-  }
 
-  function enhanceYoutubeVideoLinks($el){
-    $el.find("a[href*='youtube.com'], a[href*='youtu.be']").each(function(i){
-      var $link = $(this),
-          videoId = parseYoutubeVideoId($link.attr('href')),
-          $holder = $('<span class="media-player" />'),
-          $captions = $link.siblings('.captions');
+      function enhanceYoutubeVideoLinks($el) {
+        $el.find("a[href*='youtube.com'], a[href*='youtu.be']").each(function (i){
 
-      if(typeof videoId !== 'undefined'){
-        $link.parent().replaceWith($holder);
+          var $link = $(this);
 
-        $holder.player({
-          id: 'youtube-'+i,
-          media: videoId,
-          captions: $captions.length > 0 ? $captions.attr('href') : null,
-          url: (document.location.protocol + '//www.youtube.com/apiplayer?enablejsapi=1&version=3&playerapiid=')
+          if (shouldConvertToEmbeddedPlayer($link)) {
+            var videoId = parseYoutubeVideoId($link.attr('href'));
+
+            if(typeof videoId !== 'undefined') {
+
+              var $holder = $('<span class="media-player" />'),
+                  $captions = $link.siblings('.captions');
+
+              $link.parent().replaceWith($holder);
+              $holder.player({
+                id: 'youtube-'+i,
+                media: videoId,
+                captions: $captions.length > 0 ? $captions.attr('href') : null,
+                url: (document.location.protocol + '//www.youtube.com/apiplayer?enablejsapi=1&version=3&playerapiid=')
+              });
+            }
+          }
         });
       }
-    });
-  }
 
-  GOVUK.enhanceYoutubeVideoLinks = enhanceYoutubeVideoLinks;
+      var $element = $('.govuk-govspeak');
+      enhanceYoutubeVideoLinks($element);
 
-  $(function(){
-    GOVUK.enhanceYoutubeVideoLinks($('.govuk-govspeak'));
-  });
-})(jQuery);
+    };
+  };
+
+})(window.GOVUK.Modules);
