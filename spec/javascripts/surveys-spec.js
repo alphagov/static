alphagov/static

commit 8f1f835c8191d6128705950ecbb3750c8aaaeba9

Refactor transaction survey prevention
The correct place for this logic is inside the survey javascript where
other decisions are made with a bearing on if we should display the
survey.

During this refactor, we moved the `window.GOVUK` guard to surround what
appears to also be relevant code that should be protected by this guard.

+ @NeilvB

app/assets/javascripts/surveys.js
@@ -76,12 +76,31 @@
       if (userSurveys.otherNotificationVisible() ||
           GOVUK.cookie(userSurveys.cookieNameTakenSurvey) === 'true') {
         return false;
+      } else if (userSurveys.userCompletedTransaction()) {
+        // We don't want any survey appearing for users who have completed a
+        // transaction as they may complete the survey with the department's
+        // transaction in mind as opposed to the GOV.UK content.
+        return false;
       } else if ($('#user-satisfaction-survey-container').length <= 0) {
         return false;
       } else if (userSurveys.randomNumberMatches(survey.frequency)) {
         return true;
       } else {
         return false;
+      }
+    },
+
+    userCompletedTransaction: function() {
+      var currentURL = window.location.pathname;
+
+      function stringContains(str, substr) {
+        return str.indexOf(substr) > -1;
+      }
+
+      if (stringContains(currentURL, "/done") &&
+          stringContains(currentURL, "/transaction-finished") &&
+          stringContains(currentURL, "/driving-transaction-finished")) {
+            return true;
       }
     },
 

spec/javascripts/surveys-spec.js
@@ -37,6 +37,21 @@ describe("Surveys", function() {
     $block.remove();
   });
 
+  describe("init", function() {
+    it("shows the default survey", function() {
+      spyOn(surveys, 'randomNumberMatches').and.returnValue(true);
+      // So we're working with the user satisfaction survey, not any future small survey
+      spyOn(surveys, 'currentTime').and.returnValue(new Date("July 11, 201610:00:00").getTime());
+      surveys.init();
+
+      expect($('#take-survey').attr('href')).toContain(surveys.defaultSurvey.url);
+      expect($('#take-survey').attr('href')).toContain("?c=" + window.location.pathname);
+      expect($('#user-satisfaction-survey').length).toBe(1);
+      expect($('#user-satisfaction-survey').hasClass('visible')).toBe(true);
+      expect($('#user-satisfaction-survey').attr('aria-hidden')).toBe('false');
+    });
+  });
+
   describe("displaySurvey", function() {
     it("displays the user satisfaction div", function () {
       expect($('#user-satisfaction-survey').length).toBe(0);

app/assets/javascripts/core.js
@@ -17,31 +17,18 @@ $(document).ready(function() {
   });
 
   if (window.GOVUK) {
-    if (GOVUK.userSatisfaction){
-      var currentURL = window.location.pathname;
-
-      function stringContains(str, substr) {
-        return str.indexOf(substr) > -1;
-      }
-
-      // We don't want the satisfaction survey appearing for users who
-      // have completed a transaction as they may complete the survey with
-      // the department's transaction in mind as opposed to the GOV.UK content.
-      if (!stringContains(currentURL, "/done") &&
-          !stringContains(currentURL, "/transaction-finished") &&
-          !stringContains(currentURL, "/driving-transaction-finished")) {
-        GOVUK.userSurveys.init();
-      }
+    if (GOVUK.userSurveys){
+      GOVUK.userSurveys.init();
     }
-  }
 
-  // for radio buttons and checkboxes
-  var buttonsSelector = "label.selectable input[type='radio'], label.selectable input[type='checkbox']";
-  new GOVUK.SelectionButtons(buttonsSelector);
+    // for radio buttons and checkboxes
+    var buttonsSelector = "label.selectable input[type='radio'], label.selectable input[type='checkbox']";
+    new GOVUK.SelectionButtons(buttonsSelector);
 
-  // HMRC webchat
-  if (GOVUK.webchat) {
-    GOVUK.webchat.init();
+    // HMRC webchat
+    if (GOVUK.webchat) {
+      GOVUK.webchat.init();
+    }
   }
 });
 

commit 9558145788b48b4aeaf5e5cde8f99ce84ace7bf3

Fix transaction bug introduced in 8f1f835
Attempting to refactor the transaction detection logic, I inverted the
match (returning true if inside a transaction URL, not false if *not*
inside a transaction URL) but forgot to invert the if statement to "OR"
from "AND".

This commit fixes that, and adds some tests to ensure the mistake can't
happen again.

app/assets/javascripts/surveys.js
@@ -106,15 +106,15 @@
     },
 
     userCompletedTransaction: function() {
-      var currentURL = window.location.pathname;
+      var path = userSurveys.currentPath();
 
       function stringContains(str, substr) {
         return str.indexOf(substr) > -1;
       }
 
-      if (stringContains(currentURL, "/done") &&
-          stringContains(currentURL, "/transaction-finished") &&
-          stringContains(currentURL, "/driving-transaction-finished")) {
+      if (stringContains(path, "/done") ||
+          stringContains(path, "/transaction-finished") ||
+          stringContains(path, "/driving-transaction-finished")) {
             return true;
       }
     },
@@ -147,7 +147,8 @@
       return "govuk_" + cookieStub;
     },
 
-    currentTime: function() { return new Date().getTime(); }
+    currentTime: function() { return new Date().getTime(); },
+    currentPath: function() { return window.location.pathname; }
   };
 
   root.GOVUK.userSurveys = userSurveys;

spec/javascripts/surveys-spec.js
@@ -99,6 +99,28 @@ describe("Surveys", function() {
     });
   });
 
+  describe("userCompletedTransaction", function() {
+    it("normally returns false", function() {
+      spyOn(surveys, 'currentPath').and.returnValue('/');
+      expect(surveys.userCompletedTransaction()).toBeFalsy();
+    });
+
+    it("returns true when /done", function() {
+      spyOn(surveys, 'currentPath').and.returnValue('/done');
+      expect(surveys.userCompletedTransaction()).toBeTruthy();
+    });
+
+    it("returns true when /transaction-finished", function() {
+      spyOn(surveys, 'currentPath').and.returnValue('/transaction-finished');
+      expect(surveys.userCompletedTransaction()).toBeTruthy();
+    });
+
+    it("returns true when /driving-transaction-finished", function() {
+      spyOn(surveys, 'currentPath').and.returnValue('/driving-transaction-finished');
+      expect(surveys.userCompletedTransaction()).toBeTruthy();
+    });
+  });
+
   describe("Event handlers", function () {
       beforeEach(function() {
         surveys.displaySurvey(defaultSurvey);