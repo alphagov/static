44b31ccc264f49d75d50e6be3287074f2f08fd8f
Carlos Vilhena <carlosvilhena@gmail.com>
2016-12-14 16:19:20 +0000

    Include the new track-click module
    This will make sure we expose the `TrackClick` module to the GOVUK namespace.

--- app/assets/javascripts/start-modules.js
+++ app/assets/javascripts/start-modules.js
@@ -1,6 +1,7 @@
 //= require govuk/modules
 //= require modules/sticky-element-container
 //= require modules/toggle
+//= require modules/track-click
 
 $(document).ready(function () {
   GOVUK.modules.start();
