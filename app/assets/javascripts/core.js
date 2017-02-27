573d9db8e7dd9a794ecf067e986e3261752ead28
Paul Hayes <fofr@users.noreply.github.com>
2017-02-27 15:10:27 +0000

    Don't open print links in a new window
    * Target _blank has a vulnerability:
    https://mathiasbynens.github.io/rel-noopener/ and
    https://github.com/alphagov/whitehall/issues/2942
    * Links don’t need to be in a new window

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-  $('.print-link a').attr('target', '_blank');
-
   var $searchFocus = $('.js-search-focus');
   $searchFocus.each(function(i, el){
     if($(el).val() !== ''){
