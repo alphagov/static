f8f7cd075cbf632d00ae877212dae692b2b6bfcb
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-03-07 16:08:36 +0000

    Simplify test of analytics dimensions
    Extract a constant representing the non-dimension calls to the analytics
    spy and use it to reduce the coupling between the individual tests and
    the calls to the spy.

--- spec/javascripts/analytics/static-analytics-spec.js
+++ spec/javascripts/analytics/static-analytics-spec.js
@@ -44,6 +44,9 @@ describe("GOVUK.StaticAnalytics", function() {
     });
 
     describe('when there are govuk: meta tags', function() {
+      // The number of setup arguments which are set before the dimensions
+      const expectedDefaultArgumentCount = 5;
+
       beforeEach(function() {
         window.ga.calls.reset();
       });
@@ -67,27 +70,27 @@ describe("GOVUK.StaticAnalytics", function() {
         ');
 
         analytics = new GOVUK.StaticAnalytics({universalId: 'universal-id'});
-        universalSetupArguments = window.ga.calls.allArgs();
+        setupArguments = dimensionSetupArguments();
 
-        expect(universalSetupArguments[5]).toEqual(['set', 'dimension1', 'section']);
-        expect(universalSetupArguments[6]).toEqual(['set', 'dimension2', 'format']);
-        expect(universalSetupArguments[7]).toEqual(['set', 'dimension5', '1000']);
-        expect(universalSetupArguments[8]).toEqual(['set', 'dimension6', '2005-to-2010-labour-government']);
-        expect(universalSetupArguments[9]).toEqual(['set', 'dimension7', 'historic']);
-        expect(universalSetupArguments[10]).toEqual(['set', 'dimension9', '<D10>']);
-        expect(universalSetupArguments[11]).toEqual(['set', 'dimension10', '<W1>']);
-        expect(universalSetupArguments[12]).toEqual(['set', 'dimension32', 'accordion']);
-        expect(universalSetupArguments[13]).toEqual(['set', 'dimension33', 'navigation']);
+        expect(setupArguments[0]).toEqual(['set', 'dimension1', 'section']);
+        expect(setupArguments[1]).toEqual(['set', 'dimension2', 'format']);
+        expect(setupArguments[2]).toEqual(['set', 'dimension5', '1000']);
+        expect(setupArguments[3]).toEqual(['set', 'dimension6', '2005-to-2010-labour-government']);
+        expect(setupArguments[4]).toEqual(['set', 'dimension7', 'historic']);
+        expect(setupArguments[5]).toEqual(['set', 'dimension9', '<D10>']);
+        expect(setupArguments[6]).toEqual(['set', 'dimension10', '<W1>']);
+        expect(setupArguments[7]).toEqual(['set', 'dimension32', 'accordion']);
+        expect(setupArguments[8]).toEqual(['set', 'dimension33', 'navigation']);
       });
 
       it('ignores meta tags not set', function() {
         $('head').append('<meta name="govuk:section" content="section">');
 
         analytics = new GOVUK.StaticAnalytics({universalId: 'universal-id'});
-        universalSetupArguments = window.ga.calls.allArgs();
+        setupArguments = dimensionSetupArguments();
 
-        expect(universalSetupArguments[5]).toEqual(['set', 'dimension1', 'section']);
-        assertClosingArgumentIsAtIndex(universalSetupArguments, 6);
+        expect(setupArguments.length).toEqual(1);
+        expect(setupArguments[0]).toEqual(['set', 'dimension1', 'section']);
       });
 
       it('sets A/B meta tags as dimensions', function() {
@@ -97,10 +100,10 @@ describe("GOVUK.StaticAnalytics", function() {
         ');
 
         analytics = new GOVUK.StaticAnalytics({universalId: 'universal-id'});
-        universalSetupArguments = window.ga.calls.allArgs();
+        setupArguments = dimensionSetupArguments();
 
-        expect(universalSetupArguments[5]).toEqual(['set', 'dimension42', 'name-of-test:name-of-ab-bucket']);
-        expect(universalSetupArguments[6]).toEqual(['set', 'dimension48', 'name-of-other-test:name-of-other-ab-bucket']);
+        expect(setupArguments[0]).toEqual(['set', 'dimension42', 'name-of-test:name-of-ab-bucket']);
+        expect(setupArguments[1]).toEqual(['set', 'dimension48', 'name-of-other-test:name-of-other-ab-bucket']);
       });
 
       it('ignores dimensions outside of the A/B test range', function () {
@@ -112,11 +115,11 @@ describe("GOVUK.StaticAnalytics", function() {
         ');
 
         analytics = new GOVUK.StaticAnalytics({universalId: 'universal-id'});
-        universalSetupArguments = window.ga.calls.allArgs();
+        setupArguments = dimensionSetupArguments();
 
-        expect(universalSetupArguments[5]).toEqual(['set', 'dimension40', 'name-of-valid-test:some-bucket']);
-        expect(universalSetupArguments[6]).toEqual(['set', 'dimension49', 'name-of-other-valid-test:some-bucket']);
-        assertClosingArgumentIsAtIndex(universalSetupArguments, 7);
+        expect(setupArguments.length).toEqual(2);
+        expect(setupArguments[0]).toEqual(['set', 'dimension40', 'name-of-valid-test:some-bucket']);
+        expect(setupArguments[1]).toEqual(['set', 'dimension49', 'name-of-other-valid-test:some-bucket']);
       });
 
       it('ignores A/B meta tags with invalid dimensions', function () {
@@ -126,13 +129,14 @@ describe("GOVUK.StaticAnalytics", function() {
         ');
 
         analytics = new GOVUK.StaticAnalytics({universalId: 'universal-id'});
-        universalSetupArguments = window.ga.calls.allArgs();
+        setupArguments = dimensionSetupArguments();
 
-        assertClosingArgumentIsAtIndex(universalSetupArguments, 5);
+        expect(setupArguments.length).toEqual(0);
       });
 
-      function assertClosingArgumentIsAtIndex(setupArgs, argIndex) {
-        expect(setupArgs[argIndex]).toEqual(['send', 'pageview']);
+      function dimensionSetupArguments() {
+        // Remove the default calls to the analytics object
+        return window.ga.calls.allArgs().slice(expectedDefaultArgumentCount, -1);
       }
     });
   });
