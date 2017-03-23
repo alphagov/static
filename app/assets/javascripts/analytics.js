772befd2c7f7d5f6b83f5fd505567e02691a8874
Paul Bowsher <paul.bowsher@gmail.com>
2017-03-23 10:26:29 +0000

    Add GOV.UK analytics tracker
    Configure the GOV.UK analytics tracker with the URL for the single
    pixel gif. This is served directly by nginx’s `empty_gif` module, and
    is configured in Puppet. See
    https://github.com/alphagov/govuk_frontend_toolkit/pull/387 for details
    of the tracker.

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,4 +1,5 @@
 //= require govuk/analytics/google-analytics-universal-tracker
+//= require govuk/analytics/govuk-tracker
 //= require govuk/analytics/analytics
 //= require govuk/analytics/print-intent
 //= require govuk/analytics/error-tracking
