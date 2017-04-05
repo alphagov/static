7591a1fb62df0ab17db4685c1d0f76288aebd516
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-04-05 18:08:39 +0100

    Change how emergency banner is stored in Redis
    Rather than using a single `enabled` key to check if the emergency
    banner can be displayed, instead get the contents of an
    `emergency_banner` hash from Redis. If the contents of the hash contains
    a value for campaign class, display the banner.
    
    As we get the content for more parts of the banner, the expectation in
    `enabled?` will be updated.

--- lib/emergency_banner.rb
+++ lib/emergency_banner.rb
@@ -1,6 +1,17 @@
 class EmergencyBanner
+  def initialize
+    @redis = Redis.new
+  end
+
   def enabled?
-    redis = Redis.new
-    redis.get("emergency_banner:enabled")
+    content && has_campaign_class?
+  end
+
+  def has_campaign_class?
+    content.has_key?(:campaign_class) && content.fetch(:campaign_class).present?
+  end
+
+  def content
+    @redis.hgetall("emergency_banner")
   end
 end
