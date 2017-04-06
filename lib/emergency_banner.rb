19cf0a26934ebb3935e5653b17182ccb41550092
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-04-06 08:55:24 +0100

    Rationalise number of calls to Redis
    Set an instance variable to contain the data from Redis so we only call
    out to Redis the first time we need the data, rather than every time we
    try to access it.

--- lib/emergency_banner.rb
+++ lib/emergency_banner.rb
@@ -11,7 +11,10 @@ class EmergencyBanner
     content.has_key?(:campaign_class) && content.fetch(:campaign_class).present?
   end
 
+private
+
   def content
-    @redis.hgetall("emergency_banner")
+    @data ||= @redis.hgetall("emergency_banner")
+    @data.symbolize_keys if @data
   end
 end
