9e3523ae46f61c63f088a0c24b0a538441e9d2ff
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-04-06 09:54:53 +0100

    Add heading and tests to emergency banner
    Heading and campaign class are the two required fields for an emergency
    banner. This commit adds the heading property to Emergency Banner and
    adds tests for the various combinations of missing and empyt for
    campaign class and heading.

--- test/unit/emergency_banner_test.rb
+++ test/unit/emergency_banner_test.rb
@@ -9,22 +9,57 @@ describe "Emergency Banner" do
   context "#enabled?" do
     should "return enabled is true when all content for the emergency banner is set in Redis" do
       Redis.any_instance.stubs(:hgetall).with("emergency_banner").returns({
-          campaign_class: "black",
+        heading: "Emergency!",
+        campaign_class: "black",
         })
 
       assert @banner.enabled?
     end
 
-    should "return enabled is false when the content for the emergency_banner is missing in Redis" do
+    should "return enabled is false when the heading is not present" do
       Redis.any_instance.stubs(:hgetall).with("emergency_banner").returns({
-        campaign_class: ""
+        campaign_class: "a colour",
+        heading: "",
         })
 
       refute @banner.enabled?
     end
 
+    should "return enabled is false when the campaign_class for the emergency_banner is missing in Redis" do
+      Redis.any_instance.stubs(:hgetall).with("emergency_banner").returns({
+        campaign_class: "",
+        heading: "a heading",
+        })
+
+      refute @banner.enabled?
+    end
+
+    should "return enabled is false when the campaign_class and the heading are empty" do
+      Redis.any_instance.stubs(:hgetall).with("emergency_banner").returns({
+        campaign_class: "",
+        heading: "",
+        })
+
+      refute @banner.enabled?
+    end
     should "return enabled is false when the emergency_banner is empty in Redis" do
       Redis.any_instance.stubs(:hgetall).with("emergency_banner").returns({})
+
+      refute @banner.enabled?
+    end
+
+    should "return enabled is false when the heading is present but campaign class is not" do
+      Redis.any_instance.stubs(:hgetall).with("emergency_banner").returns({
+        heading: "a heading",
+      })
+
+      refute @banner.enabled?
+    end
+
+    should "return enabled is false when the campaign_class is present but heading class is not" do
+      Redis.any_instance.stubs(:hgetall).with("emergency_banner").returns({
+        campaign_class: "a heading",
+      })
 
       refute @banner.enabled?
     end
