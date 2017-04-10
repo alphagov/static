a45e113a062ae65fcb99944fd3116bbab374d9d9
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-04-10 16:52:11 +0100

    Pass heading to the deploy rake task
    Add heading as a required argument to the deploy rake task.

--- test/tasks/emergency_banner_test.rb
+++ test/tasks/emergency_banner_test.rb
@@ -9,12 +9,18 @@ describe 'emergency_banner' do
   should 'run the process to deploy the emergency banner' do
     EmergencyBanner::Deploy.any_instance.expects(:run)
 
-    Rake::Task['emergency_banner:deploy'].invoke('campaign class')
+    Rake::Task['emergency_banner:deploy'].invoke('campaign class', 'heading')
   end
 
   should 'raise an error if a campaign class parameter is not present' do
     assert_raises ArgumentError do
       Rake::Task['emergency_banner:deploy'].invoke
+    end
+  end
+
+  should 'raise an error if a campaign class parameter is present but a heading is not' do
+    assert_raises ArgumentError do
+      Rake::Task['emergency_banner:deploy'].invoke('campaign class')
     end
   end
 end
