3ac3d368709a634b7963382a057fc23020481eaa
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-04-10 18:27:27 +0100

    Implement Deploy the emergency banner.
    Takes up to four values and uses these to create a hash in Redis with a
    key of `emergency_banner`.

---
+++ test/unit/emergency_banner/deploy_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+require_relative '../../../lib/emergency_banner/deploy'
+
+describe "Emergency Banner::Deploy" do
+  context "#run" do
+    should "create an emergency_banner hash in Redis with a campaign_class and a heading" do
+      Redis.any_instance.expects(:hmset).with(:emergency_banner,
+                                              :campaign_class, "black",
+                                              :heading, "A title",
+                                              :short_description, "",
+                                              :link, "",
+                                             )
+
+      EmergencyBanner::Deploy.new.run("black", "A title")
+    end
+
+    should "create an emergency_banner with a campaign_class, heading and short_description" do
+      Redis.any_instance.expects(:hmset).with(:emergency_banner,
+                                              :campaign_class, "black",
+                                              :heading, "A title",
+                                              :short_description, "A short description of the event",
+                                              :link, "",
+                                             )
+
+      EmergencyBanner::Deploy.new.run("black", "A title", "A short description of the event")
+    end
+
+    should "create an emergency_banner with a campaign_class, heading and a link" do
+      Redis.any_instance.expects(:hmset).with(:emergency_banner,
+                                              :campaign_class, "black",
+                                              :heading, "A title",
+                                              :short_description, "",
+                                              :link, "https://www.gov.uk"
+                                             )
+
+      EmergencyBanner::Deploy.new.run("black", "A title", "", "https://www.gov.uk")
+    end
+
+    should "create an emergency_banner with a campaign_class, heading, short_description and a link" do
+      Redis.any_instance.expects(:hmset).with(:emergency_banner,
+                                              :campaign_class, "black",
+                                              :heading, "A title",
+                                              :short_description, "A short description of the event",
+                                              :link, "https://www.gov.uk"
+                                             )
+
+      EmergencyBanner::Deploy.new.run("black", "A title", "A short description of the event", "https://www.gov.uk")
+    end
+  end
+end
