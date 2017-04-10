f5dd40747be27f22be9835cddb668597d2bec241
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-04-10 16:47:02 +0100

    Pass campaign class to the deploy rake task
    Add campaign class as a required argument to the deploy rake task.

--- test/tasks/emergency_banner_test.rb
+++ test/tasks/emergency_banner_test.rb
@@ -2,9 +2,19 @@ require 'test_helper'
 require_relative '../../lib/emergency_banner/deploy'
 
 describe 'emergency_banner' do
+  before do
+    Rake::Task['emergency_banner:deploy'].reenable
+  end
+
   should 'run the process to deploy the emergency banner' do
     EmergencyBanner::Deploy.any_instance.expects(:run)
 
-    Rake::Task['emergency_banner:deploy'].invoke
+    Rake::Task['emergency_banner:deploy'].invoke('campaign class')
+  end
+
+  should 'raise an error if a campaign class parameter is not present' do
+    assert_raises ArgumentError do
+      Rake::Task['emergency_banner:deploy'].invoke
+    end
   end
 end
