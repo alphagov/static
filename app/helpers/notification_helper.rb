89680ab68a009aa20940e1e028ad127250552c37
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-04-26 12:12:31 +0100

    Remove old banner notification code from helper
    Also remove associated tests

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -1,18 +1,6 @@
-require "notification_file_lookup"
 require "emergency_banner/display"
 
 module NotificationHelper
-  include ActionView::Helpers::TagHelper
-
-  def banner_notification
-    if node = Static.banner
-      content_tag(:section, "<div>#{node[:file]}</div>",
-        { id: "banner-notification", class: node[:colour].to_s }, false)
-    else
-      ''
-    end
-  end
-
   def emergency_banner_notification
     emergency_banner = EmergencyBanner::Display.new
     return emergency_banner if emergency_banner.enabled?
