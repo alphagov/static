483482df989682e0cf4cc4b880684cb60b06588e
Elena Tanasoiu <elena.tanasoiu@digital.cabinet-office.gov.uk>
2017-05-16 14:28:00 +0000

    Update the icon redirects to new sizes
    Once govuk_template has been upgraded to v0.21.0 it will use a new batch of icons. The old ones no longer exist, and have been removed from the routes + tests.
    
    See https://github.com/alphagov/govuk_template/commit/82d9459596581623ca70f8d415a9117026c20414 for the changes done to icons in govuk_template.

--- app/controllers/icon_redirects_controller.rb
+++ app/controllers/icon_redirects_controller.rb
@@ -2,8 +2,4 @@ class IconRedirectsController < ApplicationController
   def show
     redirect_to view_context.asset_path(request.path.to_s[1..-1]), status: 301
   end
-
-  def apple_60_60
-    redirect_to view_context.asset_path('apple-touch-icon-60x60.png'), status: 301
-  end
 end
