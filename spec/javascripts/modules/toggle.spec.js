4fc9af50761d902e4734c8f3067e956e208242b7
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-05-18 09:50:22 +0000

    Linting to unpollute metadata pull request
    * Changes in metadata expand/collapse PR had included linting, making reading the changes difficult. This commit contains all of the linting and none of the changes, so should make that PR more readable once merged

--- spec/javascripts/modules/toggle.spec.js
+++ spec/javascripts/modules/toggle.spec.js
@@ -1,78 +1,78 @@
-describe('A toggle module', function() {
-  "use strict";
+describe('A toggle module', function () {
+  'use strict'
 
   var toggle,
-      element;
+    element
 
-  beforeEach(function() {
-    toggle = new GOVUK.Modules.Toggle();
-  });
+  beforeEach(function () {
+    toggle = new GOVUK.Modules.Toggle()
+  })
 
-  describe('when starting', function() {
+  describe('when starting', function () {
     var element = $('\
       <div>\
         <a href="#" class="my-toggle" data-expanded="false" data-controls="target">Toggle</a>\
         <div id="target">Target</div>\
-      </div>');
+      </div>')
 
-    it('adds aria attributes to toggles', function() {
-      toggle.start(element);
-      
-      var $toggle = element.find('.my-toggle');
-      expect($toggle.attr('role')).toBe('button');
-      expect($toggle.attr('aria-expanded')).toBe('false');
-      expect($toggle.attr('aria-controls')).toBe('target');
-    });
-  });
+    it('adds aria attributes to toggles', function () {
+      toggle.start(element)
 
-  describe('when clicking a toggle', function() {
-    var element;
+      var $toggle = element.find('.my-toggle')
+      expect($toggle.attr('role')).toBe('button')
+      expect($toggle.attr('aria-expanded')).toBe('false')
+      expect($toggle.attr('aria-controls')).toBe('target')
+    })
+  })
 
-    beforeEach(function() {
+  describe('when clicking a toggle', function () {
+    var element
+
+    beforeEach(function () {
       element = $('\
         <div>\
           <a href="#" class="my-toggle" data-expanded="false" data-controls="target">Toggle</a>\
           <div id="target" class="js-hidden">Target</div>\
-        </div>');
+        </div>')
 
-      toggle.start(element);
-      element.find('.my-toggle').trigger('click');
-    });
+      toggle.start(element)
+      element.find('.my-toggle').trigger('click')
+    })
 
-    it('toggles the display of a target', function() {
-      expect(element.find('#target').is('.js-hidden')).toBe(false);
-      element.find('.my-toggle').trigger('click');
-      expect(element.find('#target').is('.js-hidden')).toBe(true);
-    });
+    it('toggles the display of a target', function () {
+      expect(element.find('#target').is('.js-hidden')).toBe(false)
+      element.find('.my-toggle').trigger('click')
+      expect(element.find('#target').is('.js-hidden')).toBe(true)
+    })
 
-    it('updates the aria-expanded attribute on the toggle', function() {
-      expect(element.find('.my-toggle').attr('aria-expanded')).toBe('true');
+    it('updates the aria-expanded attribute on the toggle', function () {
+      expect(element.find('.my-toggle').attr('aria-expanded')).toBe('true')
 
-      element.find('.my-toggle').trigger('click');
-      expect(element.find('.my-toggle').attr('aria-expanded')).toBe('false');
-    });
-  });
+      element.find('.my-toggle').trigger('click')
+      expect(element.find('.my-toggle').attr('aria-expanded')).toBe('false')
+    })
+  })
 
-  describe('when clicking a toggle that controls multiple targets', function() {
-    it('toggles the display of each target', function() {
+  describe('when clicking a toggle that controls multiple targets', function () {
+    it('toggles the display of each target', function () {
       var element = $('\
         <div>\
           <a href="#" class="my-toggle" data-expanded="false" data-controls="target another-target">Toggle</a>\
           <div id="target" class="js-hidden">Target</div>\
           <div id="another-target" class="js-hidden">Another target</div>\
-        </div>');
+        </div>')
 
-      toggle.start(element);
-      expect(element.find('#target').is('.js-hidden')).toBe(true);
-      expect(element.find('#another-target').is('.js-hidden')).toBe(true);
+      toggle.start(element)
+      expect(element.find('#target').is('.js-hidden')).toBe(true)
+      expect(element.find('#another-target').is('.js-hidden')).toBe(true)
 
-      element.find('.my-toggle').trigger('click');
-      expect(element.find('#target').is('.js-hidden')).toBe(false);
-      expect(element.find('#another-target').is('.js-hidden')).toBe(false);
+      element.find('.my-toggle').trigger('click')
+      expect(element.find('#target').is('.js-hidden')).toBe(false)
+      expect(element.find('#another-target').is('.js-hidden')).toBe(false)
 
-      element.find('.my-toggle').trigger('click');
-      expect(element.find('#target').is('.js-hidden')).toBe(true);
-      expect(element.find('#another-target').is('.js-hidden')).toBe(true);
-    });
-  });
-});
+      element.find('.my-toggle').trigger('click')
+      expect(element.find('#target').is('.js-hidden')).toBe(true)
+      expect(element.find('#another-target').is('.js-hidden')).toBe(true)
+    })
+  })
+})
