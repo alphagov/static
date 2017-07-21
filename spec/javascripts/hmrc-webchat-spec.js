187011eba027a154f8f231de93cc285070162784
Murray Steele <muz@h-lame.com>
2017-07-21 13:26:20 +0000

    Add test coverage for the events sent by webchat
    We cover both the events we send to analytics and the events we send to
    the iframe that hosts the webchat.

--- spec/javascripts/hmrc-webchat-spec.js
+++ spec/javascripts/hmrc-webchat-spec.js
@@ -4,6 +4,8 @@ describe('HMRC webchat', function () {
   beforeEach(function() {
     spyOn(GOVUK.webchat, 'shouldOpen').and.returnValue(true);
     spyOn(GOVUK.webchat, 'validOrigin').and.returnValue(true);
+    spyOn(GOVUK.webchat, 'sendMessage');
+    spyOn(GOVUK.analytics, 'trackEvent');
     setFixtures(INSERTION_HOOK);
     GOVUK.webchat.init();
   });
@@ -32,6 +34,10 @@ describe('HMRC webchat', function () {
       expect($('.webchat-banner.open').length).toBe(1);
     });
 
+    it('sends an "offered" event to GA', function() {
+      expect(GOVUK.analytics.trackEvent).toHaveBeenCalledWith('webchat', 'offered');
+    });
+
   });
 
   describe('if it receives a closed message', function(){
@@ -45,6 +51,58 @@ describe('HMRC webchat', function () {
       expect($('.webchat-banner:not(.open)').length).toBe(1);
     });
 
+    it('sends no event to GA', function() {
+      expect(GOVUK.analytics.trackEvent).not.toHaveBeenCalledWith();
+    });
+
+  });
+
+  describe('if it receives an error message', function(){
+
+    beforeEach(function(done){
+      window.postMessage(JSON.stringify({evt: 'error'}), '*');
+      $(window).on('message',done);
+    });
+
+    it('sends an "error" event to GA', function() {
+      expect(GOVUK.analytics.trackEvent).toHaveBeenCalledWith('webchat', 'error');
+    });
+
+  });
+
+  describe('interacting with the webchat banner', function() {
+    beforeEach(function(done){
+      window.postMessage(JSON.stringify({evt: 'opened'}), '*');
+      $(window).on('message',done);
+    });
+
+    describe('by clicking the accept button', function() {
+      beforeEach(function() {
+        GOVUK.webchat.$banner.find('.accept').click();
+      });
+
+      it('sends an "accepted" event to GA', function() {
+        expect(GOVUK.analytics.trackEvent).toHaveBeenCalledWith('webchat', 'accepted');
+      });
+
+      it('sends an "accept" message to the iframe', function() {
+        expect(GOVUK.webchat.sendMessage).toHaveBeenCalledWith({ evt: 'accept'});
+      });
+    });
+
+    describe('by clicking the reject button', function() {
+      beforeEach(function() {
+        GOVUK.webchat.$banner.find('.reject').click();
+      });
+
+      it('sends an "rejected" event to GA', function() {
+        expect(GOVUK.analytics.trackEvent).toHaveBeenCalledWith('webchat', 'rejected');
+      });
+
+      it('sends an "reject" message to the iframe', function() {
+        expect(GOVUK.webchat.sendMessage).toHaveBeenCalledWith({ evt: 'reject'});
+      });
+    });
   });
 
 });
