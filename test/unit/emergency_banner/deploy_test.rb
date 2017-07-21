aaa26af69464184b58806f5c514fa57205ae83ed
Bevan Loon <bevan.loon@digital.cabinet-office.gov.uk>
2017-07-21 11:24:58 +0100

    Add `link_text` param to EmergencyBanner::Deploy
    Emergency banners have the option of displaying a hyperlink for more
    information. The hyperlink text is currently hard coded to "More
    information" in the view. We are changing this so that the text can be
    user defined.
    
    This commit sets the groundwork by writing an additional `link_text`
    parameter to Redis to hold the text we wish to display for the
    hyperlink.

--- test/unit/emergency_banner/deploy_test.rb
+++ test/unit/emergency_banner/deploy_test.rb
@@ -9,6 +9,7 @@ describe "Emergency Banner::Deploy" do
                                               :heading, "A title",
                                               :short_description, "",
                                               :link, "",
+                                              :link_text, "",
                                              )
 
       EmergencyBanner::Deploy.new.run("notable-death", "A title")
@@ -20,31 +21,46 @@ describe "Emergency Banner::Deploy" do
                                               :heading, "A title",
                                               :short_description, "A short description of the event",
                                               :link, "",
+                                              :link_text, "",
                                              )
 
       EmergencyBanner::Deploy.new.run("notable-death", "A title", "A short description of the event")
     end
 
-    should "create an emergency_banner with a campaign_class, heading and a link" do
+    should "create an emergency_banner with a campaign_class, heading and link" do
       Redis.any_instance.expects(:hmset).with(:emergency_banner,
                                               :campaign_class, "notable-death",
                                               :heading, "A title",
                                               :short_description, "",
-                                              :link, "https://www.gov.uk"
+                                              :link, "https://www.gov.uk",
+                                              :link_text, ""
                                              )
 
       EmergencyBanner::Deploy.new.run("notable-death", "A title", "", "https://www.gov.uk")
     end
 
-    should "create an emergency_banner with a campaign_class, heading, short_description and a link" do
+    should "create an emergency_banner with a campaign_class, heading, short_description and link" do
       Redis.any_instance.expects(:hmset).with(:emergency_banner,
                                               :campaign_class, "notable-death",
                                               :heading, "A title",
                                               :short_description, "A short description of the event",
-                                              :link, "https://www.gov.uk"
+                                              :link, "https://www.gov.uk",
+                                              :link_text, ""
                                              )
 
       EmergencyBanner::Deploy.new.run("notable-death", "A title", "A short description of the event", "https://www.gov.uk")
+    end
+
+    should "create an emergency_banner with a campaign_class, heading, short_description, link and link_text" do
+      Redis.any_instance.expects(:hmset).with(:emergency_banner,
+                                              :campaign_class, "notable-death",
+                                              :heading, "A title",
+                                              :short_description, "A short description of the event",
+                                              :link, "https://www.gov.uk",
+                                              :link_text, "Text for hyperlink",
+                                             )
+
+      EmergencyBanner::Deploy.new.run("notable-death", "A title", "A short description of the event", "https://www.gov.uk", "Text for hyperlink")
     end
   end
 end
