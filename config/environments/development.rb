911f778af86579727ff8015ab1d1fe368ab5b120
Paul Hayes <fofr@users.noreply.github.com>
2017-08-02 15:17:54 +0100

    Update boilerplate to Rails 5.1
    Run `rails app:update` and take on minimal changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,8 @@
-Static::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
+  # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
@@ -30,7 +30,7 @@ Static::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  # Disable logging of assets
+  # Suppress logger output for asset requests.
   config.assets.quiet = true
 
   config.eager_load = false
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,5 @@
-Static::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
   # test suite.  You never need to work with it otherwise.  Remember that
