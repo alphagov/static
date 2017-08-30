3bb622b47911860c55242eb608c2035cabdad887
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-08-30 08:25:40 +0000

    Improve next previous accessibility
    - hidden pseudo element on label element containing "." causes screen readers to take a breath between navigation and label parts of link
    - tested on Voiceover (Mac), Narrator, Jaws 18, NVDA (Windows)
    - unable to test for pseudo element content so specific tests for accessibility removed, but comment added to SCSS to prevent accidental removal

--- test/govuk_component/previous_and_next_navigation_test.rb
+++ test/govuk_component/previous_and_next_navigation_test.rb
@@ -33,38 +33,4 @@ class PreviousAndNextNavigationTestCase < ComponentTestCase
     assert_select ".pub-c-pagination__link-label", text: "2 of 3"
     assert_link("next-page")
   end
-
-  test "there is a distinction between the navigation text and label text of the links when a label is present" do
-    render_component(
-      previous_page: {
-        url: "previous-page",
-        title: "Previous page",
-        label: "1 of 3"
-      },
-      next_page: {
-        url: "next-page",
-        title: "Next page",
-        label: "2 of 3"
-      }
-    )
-
-    assert_select ".pub-c-pagination__item--previous .pub-c-pagination__link .visually-hidden", text: ":"
-    assert_select ".pub-c-pagination__item--next .pub-c-pagination__link .visually-hidden", text: ":"
-  end
-
-  test "there is no distinction between the navigation text and label text of the links when labels are not present" do
-    render_component(
-      previous_page: {
-        url: "previous-page",
-        title: "Previous page"
-      },
-      next_page: {
-        url: "next-page",
-        title: "Next page"
-      }
-    )
-
-    assert_select ".pub-c-pagination__item--previous .pub-c-pagination__link .visually-hidden", false
-    assert_select ".pub-c-pagination__item--next .pub-c-pagination__link .visually-hidden", false
-  end
 end
