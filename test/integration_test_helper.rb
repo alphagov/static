fc66f42d01aa7be8721e56311dd66a0f834170f4
Paul Hayes <fofr@users.noreply.github.com>
2017-09-21 18:01:12 +0100

    Increase poltergeist timeout
    30s isn’t long enough to compile all the static assets. Increase to 2
    minutes to allow tests to run.
    
    Switch from require phantomjs/poltergeist
    (https://github.com/colszowka/phantomjs-gem/blob/master/lib/phantomjs/po
    ltergeist.rb) to a modified version that specifies a timeout.

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -2,10 +2,15 @@ require_relative 'test_helper'
 require 'rack/test'
 require 'capybara/rails'
 require 'capybara/poltergeist'
+require 'phantomjs'
 
 # TODO: Remove when PhantomJS updated on Puppet
-# Include via phantomjs to use modern PhantomJS version
-require 'phantomjs/poltergeist'
+# Use modern PhantomJS version via gem
+# Based on: https://github.com/colszowka/phantomjs-gem/blob/master/lib/phantomjs/poltergeist.rb
+Phantomjs.path
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, phantomjs: Phantomjs.path, timeout: 2.minutes)
+end
 
 Capybara.javascript_driver = :poltergeist
 
