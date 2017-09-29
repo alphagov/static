6cf19c0c06fefd03571e2eee8509ee79486a1ffc
Paul Hayes <fofr@users.noreply.github.com>
2017-09-29 11:46:12 +0100

    Allow context in titles to be a link
    * Allows titles in Collections app to switch to the title component
    * It’s not clear how useful this feature is and if users are clicking
    on it, include data attributes to allow tracking and include caveat in
    the docs
    * Use API that matches HTML
    * Keep backwards compatibility with setting context to a string
    * Use black focus styles to match breadcrumbs

--- test/govuk_component/title_test.rb
+++ test/govuk_component/title_test.rb
@@ -21,6 +21,11 @@ class TitleComponentTestCase < ComponentTestCase
     assert_select ".pub-c-title__context", text: "Format"
   end
 
+  test "title context link appears" do
+    render_component(title: "Hello World", context: { text: "Format", href: "/format", data: { tracking: true } })
+    assert_select ".pub-c-title__context-link[href='/format'][data-tracking]", text: "Format"
+  end
+
   test "applies title length if supplied" do
     render_component(title: "Hello World", context: "format", average_title_length: 'long')
     assert_select ".pub-c-title .pub-c-title__text--long", text: "Hello World"
