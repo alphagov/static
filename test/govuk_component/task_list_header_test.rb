cdc4b5e19877f1bdde7471d9b5a99099e8900046
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-10-31 14:21:27 +0000

    Rename component
    - tasklist now called task list, updating this component to same

--- test/govuk_component/task_list_header_test.rb
+++ test/govuk_component/task_list_header_test.rb
@@ -1,8 +1,8 @@
 require 'govuk_component_test_helper'
 
-class TasklistHeaderTestCase < ComponentTestCase
+class TaskListHeaderTestCase < ComponentTestCase
   def component_name
-    "tasklist_header"
+    "task_list_header"
   end
 
   test "renders nothing without passed content" do
@@ -12,24 +12,24 @@ class TasklistHeaderTestCase < ComponentTestCase
   test "renders default component" do
     render_component(title: "This is my title")
 
-    assert_select ".pub-c-tasklist-header span.pub-c-tasklist-header__title", text: "This is my title"
+    assert_select ".pub-c-task-list-header span.pub-c-task-list-header__title", text: "This is my title"
   end
 
   test "renders with a link" do
     render_component(title: "This is my title", path: "/notalink")
 
-    assert_select ".pub-c-tasklist-header a.pub-c-tasklist-header__title[href='/notalink']", text: "This is my title"
+    assert_select ".pub-c-task-list-header a.pub-c-task-list-header__title[href='/notalink']", text: "This is my title"
   end
 
   test "renders with a skip link" do
     render_component(title: "This is my title", skip_link: "#skiplink")
 
-    assert_select ".pub-c-tasklist-header .pub-c-tasklist-header__skip-link[href='#skiplink']", text: "Skip content"
+    assert_select ".pub-c-task-list-header .pub-c-task-list-header__skip-link[href='#skiplink']", text: "Skip content"
   end
 
   test "renders with a skip link with custom text" do
     render_component(title: "This is my title", skip_link: "#skiplink", skip_link_text: "It's hard to think of a good value")
 
-    assert_select ".pub-c-tasklist-header .pub-c-tasklist-header__skip-link[href='#skiplink']", text: "It's hard to think of a good value"
+    assert_select ".pub-c-task-list-header .pub-c-task-list-header__skip-link[href='#skiplink']", text: "It's hard to think of a good value"
   end
 end
