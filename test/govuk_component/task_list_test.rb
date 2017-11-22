e5f6dbf828f367d5d2858b05ad09e838137b22de
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-11-22 16:41:25 +0000

    Add links option to component

--- test/govuk_component/task_list_test.rb
+++ test/govuk_component/task_list_test.rb
@@ -14,6 +14,20 @@ class TaskListTest < ComponentTestCase
             {
               type: 'paragraph',
               text: 'This is the first paragraph'
+            },
+            {
+              type: 'list',
+              links: [
+                {
+                  path: '/link1',
+                  text: 'Link 1',
+                },
+                {
+                  path: '/link2',
+                  text: 'Link 2',
+                  cost: '&pound;0 to &pound;300'
+                },
+              ]
             }
           ]
         }
@@ -25,6 +39,15 @@ class TaskListTest < ComponentTestCase
             {
               type: 'paragraph',
               text: 'This is the second paragraph'
+            },
+            {
+              type: 'list',
+              links: [
+                {
+                  path: '/link3',
+                  text: 'Link 3',
+                },
+              ]
             }
           ]
         }
@@ -39,16 +62,22 @@ class TaskListTest < ComponentTestCase
     assert_empty render_component({})
   end
 
-  test "renders a simple tasklist correctly" do
+  test "renders a task list with paragraphs and links" do
     render_component(groups: simple_tasklist)
     assert_select ".pub-c-task-list"
 
     assert_select group1 + " .pub-c-task-list__step#first-header"
     assert_select group1 + " .pub-c-task-list__title", text: "First header"
-    assert_select group1 + " .pub-c-task-list__panel", text: "This is the first paragraph"
+    assert_select group1 + " .pub-c-task-list__paragraph", text: "This is the first paragraph"
 
     assert_select group2 + " .pub-c-task-list__step#second-header"
     assert_select group2 + " .pub-c-task-list__title", text: "Second header"
-    assert_select group2 + " .pub-c-task-list__panel", text: "This is the second paragraph"
+    assert_select group2 + " .pub-c-task-list__paragraph", text: "This is the second paragraph"
+
+    assert_select group1 + " .pub-c-task-list__link-item[href='/link1']", text: "Link 1"
+    assert_select group1 + " .pub-c-task-list__link-item[href='/link2']", text: "Link 2"
+    assert_select group1 + " .pub-c-task-list__cost", text: "&pound;0 to &pound;300"
+
+    assert_select group2 + " .pub-c-task-list__link-item[href='/link3']", text: "Link 3"
   end
 end
