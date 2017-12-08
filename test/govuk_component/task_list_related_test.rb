03438b53f7a41aed85cfbed465f86a2543bbbfb1
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2017-12-08 11:17:59 +0000

    Add tracking to component

--- test/govuk_component/task_list_related_test.rb
+++ test/govuk_component/task_list_related_test.rb
@@ -34,21 +34,36 @@ class TaskListRelatedTestCase < ComponentTestCase
   test "displays one link inside a heading" do
     render_component(links: one_link)
 
+    this_link = ".pub-c-task-list-related .pub-c-task-list-related__heading .pub-c-task-list-related__link"
+
     assert_select ".pub-c-task-list-related .pub-c-task-list-related__heading .pub-c-task-list-related__pretitle", text: 'Part of'
-    assert_select ".pub-c-task-list-related .pub-c-task-list-related__heading .pub-c-task-list-related__link[href='/link1']", text: 'Link 1'
+    assert_select this_link + "[href='/link1']", text: 'Link 1'
+    assert_select this_link + "[data-track-category='tasklistPartOfClicked']"
+    assert_select this_link + "[data-track-action='Part of']"
+    assert_select this_link + "[data-track-label='/link1']"
+    assert_select this_link + "[data-track-dimension='Link 1']"
+    assert_select this_link + "[data-track-dimension-index='29']"
   end
 
   test "displays more than one link in a list" do
     render_component(links: two_links)
 
+    this_link = ".pub-c-task-list-related .pub-c-task-list-related__links .pub-c-task-list-related__link[href='/link2']"
+
     assert_select ".pub-c-task-list-related .pub-c-task-list-related__heading .pub-c-task-list-related__pretitle", text: 'Part of'
     assert_select ".pub-c-task-list-related .pub-c-task-list-related__links .pub-c-task-list-related__link[href='/link1']", text: 'Link 1'
-    assert_select ".pub-c-task-list-related .pub-c-task-list-related__links .pub-c-task-list-related__link[href='/link2']", text: 'Link 2'
+    assert_select this_link, text: 'Link 2'
+    assert_select this_link + "[data-track-category='tasklistPartOfClicked']"
+    assert_select this_link + "[data-track-action='Part of']"
+    assert_select this_link + "[data-track-label='/link2']"
+    assert_select this_link + "[data-track-dimension='Link 2']"
+    assert_select this_link + "[data-track-dimension-index='29']"
   end
 
   test "alternative heading text" do
     render_component(links: one_link, pretitle: 'Moo')
 
-    assert_select ".pub-c-task-list-related .pub-c-task-list-related__heading .pub-c-task-list-related__pretitle", text: 'Moo'
+    assert_select ".pub-c-task-list-related__pretitle", text: 'Moo'
+    assert_select ".pub-c-task-list-related__link[data-track-action='Moo']"
   end
 end
