6c537e33fd7d7a23e19f8db39425ef975aa43620
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2018-04-03 10:47:00 +0000

    Rename classes to use BEM

--- test/govuk_component/search_test.rb
+++ test/govuk_component/search_test.rb
@@ -7,39 +7,39 @@ class SearchTestCase < ComponentTestCase
 
   test "renders a search box with default options" do
     render_component({})
-    assert_select ".govuk-search.on-white"
+    assert_select ".pub-c-search.pub-c-search--on-white"
   end
 
   test "renders a search box for a dark background" do
     render_component(on_govuk_blue: true)
-    assert_select ".govuk-search.on-govuk-blue"
+    assert_select ".pub-c-search.pub-c-search--on-govuk-blue"
   end
 
   test "renders a search box with a custom label text" do
     render_component(label_text: "This is my new label")
-    assert_select ".govuk-search .search-label", text: "This is my new label"
+    assert_select ".pub-c-search .pub-c-search__label", text: "This is my new label"
   end
 
   test "renders a search box with a custom label content" do
     render_component(inline_label: false, label_text: "<h1>This is a heading 1</h1>")
-    assert_select ".govuk-search .search-label h1", text: "This is a heading 1"
-    assert_select ".govuk-search.search-separate-label"
+    assert_select ".pub-c-search .pub-c-search__label h1", text: "This is a heading 1"
+    assert_select ".pub-c-search.pub-c-search--separate-label"
   end
 
   test "renders a search box with a value" do
     render_component(value: "I searched for this")
-    assert_select ".govuk-search .search-input" do
+    assert_select ".pub-c-search .pub-c-search__input" do
       assert_select "[value=?]", "I searched for this"
     end
   end
 
   test "renders a search box with a custom id" do
     render_component(id: "my-unique-id")
-    assert_select ".govuk-search #my-unique-id.search-input"
+    assert_select ".pub-c-search #my-unique-id.pub-c-search__input"
   end
 
   test "renders a large search box" do
     render_component(size: "large")
-    assert_select ".govuk-search.search-large"
+    assert_select ".pub-c-search.pub-c-search--large"
   end
 end
