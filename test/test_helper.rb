d7f363b75f474e5a115cc8c8f168de9b1b5795ac
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-09-17 09:27:28 +0100

    Fix linting errors

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 
 require 'minitest/autorun'
 require 'test/unit'
