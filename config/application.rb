51653e27fae2e69eaa5ba554e9d87191d3f084e6
Kevin Dew <kevindew@me.com>
2019-02-18 11:38:39 +0000

    Update govuk_publishing_components gem
    This can now be updated since we aren't trying to precompile the
    node_modules directory.
    
    As slimmer is no longer a dependency of govuk_publishing_components we
    can remove the slimmer disabling from config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -54,12 +54,5 @@ module Static
 
     # Google Analytics ID
     config.ga_universal_id = ENV.fetch("GA_UNIVERSAL_ID", "UA-UNSET")
-
-    # Slimmer is used by the govuk_publishing_components gem, however it inserts itself
-    # automatically as middleware in the host Rails application
-    # Disable Slimmer middleware for Static and enable for use in gem only
-    if defined?(GovukPublishingComponents)
-      config.middleware.delete Slimmer::App
-    end
   end
 end
