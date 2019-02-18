38f92808fd7c2b14060252c1f7f6d022d27bd1f4
Kevin Dew <kevindew@me.com>
2019-02-18 19:36:14 +0000

    Make barchart govspeak enhancement resilient to re-initialisation
    This barchart initialisation is vulnerable to breaking a component if
    the initialisation is called twice. As the code for this is moving to
    govuk_publishing_components there is going to be a period of time where
    this can quite easily occur while apps are being updated and deployed.
    
    This updates this initialisation code to mark when a chart has been
    initialised and be able to ignore being run on charts that have already
    been initialised.

--- app/assets/javascripts/govuk-component/govspeak-convert-html-pub-charts.js
+++ app/assets/javascripts/govuk-component/govspeak-convert-html-pub-charts.js
@@ -1,8 +1,9 @@
 function govspeakBarcharts() {
-    $('.govuk-govspeak .js-barchart-table').each(function() {
+    $('.govuk-govspeak .js-barchart-table:not(.mc-chart):not(.js-barchart-table-init)').each(function() {
         $.magnaCharta($(this), {
             toggleText: "Change between chart and table"
         });
+      $(this).addClass('js-barchart-table-init')
     })
 }
 $(govspeakBarcharts);
