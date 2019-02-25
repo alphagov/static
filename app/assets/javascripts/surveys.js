e32e96ba6d040f82abc04fdb567ae7b6d87324dc
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2019-02-25 12:32:47 +0000

    Add start/end dates to service standard survey

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -97,8 +97,8 @@
         identifier: 'service_standards_reports_survey',
         surveyType: 'url',
         frequency: 1,
-        startTime: new Date("February 07, 2019").getTime(),
-        endTime: new Date("April 07, 2019").getTime(),
+        startTime: new Date("February 26, 2019").getTime(),
+        endTime: new Date("March 27, 2019").getTime(),
         url: 'https://www.smartsurvey.co.uk/s/SurveyStandardReportsPage/',
         templateArgs: {
           title: "Help us improve Service Standards Reports on GOV.UK",
