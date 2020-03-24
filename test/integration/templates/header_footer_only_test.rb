1a75dc2d6b73173da40dbc525a0b3a9d854e3ea6
Kelvin Gan <dev@kelvingan.co.uk>
2020-03-24 10:33:02 +0000

    Revert "Revert "Revert "Remove (temporarily) the search box from header"""

--- test/integration/templates/header_footer_only_test.rb
+++ test/integration/templates/header_footer_only_test.rb
@@ -11,10 +11,9 @@ class HeaderFooterOnlyTest < ActionDispatch::IntegrationTest
     end
 
     within "body" do
-      # TODO: This is temporary
-      # within "header#global-header" do
-      #   assert page.has_selector?("form#search")
-      # end
+      within "header#global-header" do
+        assert page.has_selector?("form#search")
+      end
 
       assert page.has_selector?("#global-cookie-message")
       assert page.has_selector?("#user-satisfaction-survey-container")
