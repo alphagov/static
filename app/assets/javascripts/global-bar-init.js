2bff1a0c8097bd9375d064980994a8d5050d0d63
Simon <sihugh@users.noreply.github.com>
2020-05-09 15:17:17 +0100

    Revert "Reset global banner"

--- app/assets/javascripts/global-bar-init.js
+++ app/assets/javascripts/global-bar-init.js
@@ -6,7 +6,7 @@ window.GOVUK = window.GOVUK || {}
 
 // Bump this if you are releasing a major change to the banner
 // This will reset the view count so all users will see the banner, even if previously seen
-var BANNER_VERSION = 7;
+var BANNER_VERSION = 6;
 var GLOBAL_BAR_SEEN_COOKIE = "global_bar_seen"
 
 var globalBarInit = {
