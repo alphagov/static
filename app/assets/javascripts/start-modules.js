269550ae100c18f9bfa94fe76b435034a3a7a959
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2020-05-28 16:54:29 +0100

    Call cookie banner component JS

--- app/assets/javascripts/start-modules.js
+++ app/assets/javascripts/start-modules.js
@@ -1,4 +1,5 @@
 // = require govuk_publishing_components/modules
+// = require govuk_publishing_components/components/cookie-banner
 // = require modules/global-bar
 // = require modules/sticky-element-container
 // = require modules/toggle
