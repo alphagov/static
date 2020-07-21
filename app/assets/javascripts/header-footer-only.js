09c361ba4613dca36fbaed0f8e855537a4ccb7da
Andrew White <andrew.white@unboxed.co>
2020-07-21 22:14:49 +0100

    Load files in correct order
    The core.js file needs GOVUK.SelectionButtons so move after the require.

--- app/assets/javascripts/header-footer-only.js
+++ app/assets/javascripts/header-footer-only.js
@@ -2,6 +2,6 @@
 //= require vendor/polyfills/bind
 //= require analytics
 //= require start-modules
-//= require core
 //= require govuk/selection-buttons
 //= require govuk/shim-links-with-button-role
+//= require core
