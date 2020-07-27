5409d8e52000f035d40523b2c90ab7c284582663
Andrew White <andrew.white@unboxed.co>
2020-07-27 18:59:56 +0100

    Fix asset pipeline with libsass
    Using a sass css compressor causes a scss file to be processed twice
    (once to build, once to compress) which breaks the usage of "unquote"
    to use CSS that has same function names as SCSS such as max.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,10 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Rather than use a CSS compressor, use the SASS style to perform compression.
+  config.sass.style = :compressed
+  config.sass.line_comments = false
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
