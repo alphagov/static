cfb362b3fd9902c7c33a1c32e2503aaeaf49ca4c
Alex <alex-ju@users.noreply.github.com>
2021-02-16 17:45:03 +0000

    Remove sticky-element-container script and style
    This is only being used in `government-frontnend` so we're moving it there instead.

--- app/assets/javascripts/start-modules.js
+++ app/assets/javascripts/start-modules.js
@@ -2,7 +2,6 @@
 //= require govuk_publishing_components/components/cookie-banner
 //= require govuk_publishing_components/lib/track-click
 //= require modules/global-bar
-//= require modules/sticky-element-container
 //= require modules/cross-domain-tracking
 
 $(document).ready(function () {
