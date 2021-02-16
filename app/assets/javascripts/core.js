12ec9dd909a033b64c58f8856f34b79fc1f215b6
Alex <alex-ju@users.noreply.github.com>
2021-02-16 19:44:56 +0000

    Remove js-search-focus
    No evidence of this being used across frontend applications

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -1,20 +1,3 @@
 $(document).ready(function () {
   $('.print-link a').attr('target', '_blank')
-
-  var $searchFocus = $('.js-search-focus')
-  $searchFocus.each(function (i, el) {
-    if ($(el).val() !== '') {
-      $(el).addClass('focus')
-    }
-  })
-
-  $searchFocus.on('focus', function (e) {
-    $(e.target).addClass('focus')
-  })
-
-  $searchFocus.on('blur', function (e) {
-    if ($(e.target).val() === '') {
-      $(e.target).removeClass('focus')
-    }
-  })
 })
