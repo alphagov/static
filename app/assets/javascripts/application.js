e77d84469c075f499c0bb0b920ffc474ad50e0d0
Alex <alex-ju@users.noreply.github.com>
2021-03-04 14:22:05 +0000

    Remove frontend toolkit JS imports

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,1 @@
 //= require header-footer-only
-//= require govuk_toolkit
--- app/assets/javascripts/header-footer-only.js
+++ app/assets/javascripts/header-footer-only.js
@@ -1,5 +1,4 @@
 //= require libs/jquery/plugins/jquery.base64
-//= require vendor/polyfills/bind
 //= require analytics
 //= require start-modules
 //= require govuk_publishing_components/components/layout-header
