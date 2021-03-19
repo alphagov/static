783d65281709c7c03c0a530fc6efa89de9be2a68
Ian James <ian.james@digital.cabinet-office.gov.uk>
2021-03-19 11:34:53 +0000

    Update error pages to use public layout template
     * The base error page template updated to use the public layout component
     * Application stylesheet has been updated to include only the styles needed
       for the public layout component, global bar, and emergency banner.
     * Application JavaScript has been updated to only include the modules needed
       for the public layout component
     * Error page stylesheet has only the extra imports needed - in this case, only
       the the title component was needed
     * Minor tweak to make the apostrophe in the "We're" on some of the error pages
       look nicer
     * Updated tests to match content and layout changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,17 @@
-//= require header-footer-only
+//= require govuk_publishing_components/dependencies
+
+//= require govuk_publishing_components/lib/cookie-functions
+//= require govuk_publishing_components/lib/header-navigation
+//= require govuk_publishing_components/lib/track-click
+
+//= require analytics
+
+//= require govuk_publishing_components/components/cookie-banner
+//= require govuk_publishing_components/components/layout-header
+//= require govuk_publishing_components/components/feedback
+
+//= require modules/global-bar
+//= require modules/cross-domain-tracking
+
+//= require global-bar-init
+//= require surveys
--- test/integration/templates/error_5xx_test.rb
+++ test/integration/templates/error_5xx_test.rb
@@ -4,28 +4,27 @@ class Error5XXTest < ActionDispatch::IntegrationTest
   should "render the 500 template" do
     visit "/templates/500.html.erb"
 
-    assert page.has_selector?("body.mainstream.error")
     within "head", visible: :all do
-      assert page.has_selector?("title", text: "Sorry, we're experiencing technical difficulties - GOV.UK", visible: :all)
+      assert page.has_selector?("title", text: "Sorry, we’re experiencing technical difficulties - GOV.UK", visible: :all)
       assert page.has_selector?("link[href$='application.css']", visible: :all)
     end
 
-    within "body.mainstream.error" do
-      within "header#global-header" do
+    within "body" do
+      within "header.gem-c-layout-header.govuk-header" do
         assert page.has_selector?("form#search")
       end
 
       assert page.has_selector?("#global-cookie-message")
       assert page.has_selector?("#user-satisfaction-survey-container")
 
-      within "#wrapper" do
-        assert page.has_selector?("h1", text: "Sorry, we're experiencing technical difficulties")
+      within "#content" do
+        assert page.has_selector?("h1", text: "Sorry, we’re experiencing technical difficulties")
         assert page.has_selector?("pre", text: "Status code: 500", visible: :all)
       end
 
       within "footer" do
-        assert page.has_selector?(".footer-categories")
-        assert page.has_selector?(".footer-meta")
+        assert page.has_selector?(".govuk-footer__navigation")
+        assert page.has_selector?(".govuk-footer__meta")
       end
     end
 
