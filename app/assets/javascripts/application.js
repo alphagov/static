c408f6aa0becc4e4185b2122543c67c2ecbc346d
Alex J <alexandru.jurubita@digital.cabinet-office.gov.uk>
2022-02-22 11:26:51 +0000

    Add skip-link script
    `govuk_publishing_components` v28.6 (https://github.com/alphagov/static/pull/2712) brings in a `skip-link` script that needs to be imported by `static`

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,9 +6,10 @@
 //= require analytics
 
 //= require govuk_publishing_components/components/cookie-banner
+//= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/layout-super-navigation-header
-//= require govuk_publishing_components/components/feedback
+//= require govuk_publishing_components/components/skip-link
 
 //= require modules/global-bar
 //= require modules/cross-domain-tracking
