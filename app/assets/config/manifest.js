3104907a0063dbd882ade08f2fc2719bb6797d24
Kevin Dew <kevindew@me.com>
2022-03-24 11:56:49 +0000

    Switch configuration for Sprockets 4
    Sprockets 4 requires a manifest.js file and prefers this to using the
    config.assets.precompile array.

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+//= link_tree ../images
+//
+// we want to compile all the root files in stylesheets
+//= link_directory ../stylesheets .css
+
+//= link application.js
+//= link error-page.js
+//= link global-bar-init.js
+//= link modules/base-target.js
+//= link surveys.js
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,20 +5,3 @@ Rails.application.config.assets.version = "1.0"
 
 # Compile assets to a location that doesn't conflict with upstream requests
 Rails.application.config.assets.prefix = "/assets/static"
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-Rails.application.config.assets.precompile += %w[
-  favicon.ico
-  application.js
-  error-page.js
-  global-bar-init.js
-  modules/base-target.js
-  surveys.js
-  application*.css
-  error-page-print.css
-  error-page.css
-  guides-print.css
-  print.css
-]
