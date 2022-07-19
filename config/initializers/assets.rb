180cf26191ad76db158b1f6914f1dca073576bfc
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2022-07-19 16:32:17 +0100

    Update to rails 7 with `rails app:update`
    Maintained custom config;
     - in dev: hosts, asset_host
     - in prod:
        - assets.js_compressor,
        - sass
        - action_controller.asset_host
        - Google Analytics ID
        - heroku for preview
      - in test: assets.digest

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,11 @@ Rails.application.config.assets.version = "1.0"
 
 # Compile assets to a location that doesn't conflict with upstream requests
 Rails.application.config.assets.prefix = "/assets/static"
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
