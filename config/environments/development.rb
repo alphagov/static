8ad229d1383473d17474a93621d0472b57d35971
Kevin Dew <kevindew@me.com>
2022-12-28 20:19:14 +0000

    Update dev config for asset proxying
    This adjusts this application so that the static asset proxying [1] can
    be used as the default approach for handling assets in this application.
    
    The motivation for this is an upcoming change in the GOV.UK Content
    Security Policy which restricts the hosts used for assets [2]. This
    change offers an opportunity to make the app behave more consistently
    with production.
    
    [1]: https://github.com/alphagov/govuk_app_config/blob/7f060692720df50a27f6845f052b04eae2246226/lib/govuk_app_config/govuk_proxy/static_proxy.rb
    [2]: https://github.com/alphagov/govuk_app_config/pull/274

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,20 +45,15 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Allow overriding the asset host with an environment variable.
-  config.asset_host = ENV["ASSET_HOST"] || Plek.new.find("static")
-
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  # Allow access to the application using dev.gov.uk hostnames
-  config.hosts += %w[
-    draft-static.dev.gov.uk
-    static.dev.gov.uk
-  ]
+  # Allow requests for all domains e.g. <app>.dev.gov.uk
+  config.hosts.clear
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
