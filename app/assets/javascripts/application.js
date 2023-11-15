01203f409a21f05f55c11156b0ddf0f480ee2704
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2023-11-15 16:42:59 +0000

    Add 'cross service header' component assets
    Co-Authored-By: Martin Jones <28779939+MartinJJones@users.noreply.github.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,7 @@
 
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/cookie-banner
+//= require govuk_publishing_components/components/cross-service-header
 //= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/layout-super-navigation-header
