3423e335b11154fa57c814d45ece336e19b4bcba
Jon Kirwan <87758239+jon-kirwan@users.noreply.github.com>
2023-11-23 12:43:50 +0000

    Configure builds
    - Create `dartsass.builds` initializer and add all Sass files to be compiled. See https://github.com/rails/dartsass-rails#configuring-builds
    - Create .keep
    - Update manifest file to use /builds directory
    - Ignore /builds
    
    Co-Authored-By: Martin Jones <28779939+MartinJJones@users.noreply.github.com>

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,5 @@
 //= link_tree ../images
 //= link application.js
 //= link test-dependencies.js
-//= link application-ie8.css
-//= link application-print.css
-//= link application.css
-//= link guides-print.css
-//= link print.css
 //= link modules/base-target.js
+//= link_tree ../builds
