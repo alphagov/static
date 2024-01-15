f2fcf8bcc08c3dbc76dbde8b97636c0827233b95
Jonathon <jonathon.shire@digital.cabinet-office.gov.uk>
2024-01-15 11:13:03 +0000

    Enable Rails 7.1 config
    Rails 7.1 was updated in the gemfiles and dependencies but was not
    enabled in the application.rb config. This pull request fixes this
    and should complete the rails 7.1 upgrade.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,9 +3,6 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
-# Compile assets to a location that doesn't conflict with upstream requests
-Rails.application.config.assets.prefix = "/assets/static"
-
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
