081804ef3865a61e80e59556ce9415def59305bf
AshGDS <8880610+AshGDS@users.noreply.github.com>
2024-09-02 12:39:45 +0100

    Move es6-components.js back into application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,13 @@
 // depend on any modules from govuk-frontend so can be
 // included here:
 //
+//= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/cookie-banner
 //= require govuk_publishing_components/components/cross-service-header
 //= require govuk_publishing_components/components/feedback
+//= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/layout-super-navigation-header
+//= require govuk_publishing_components/components/skip-link
 
 //= require modules/global-bar
 
--- config/initializers/govuk_publishing_components.rb
+++
@@ -1,3 +0,0 @@
-GovukPublishingComponents.configure do |config|
-  config.use_es6_components = true
-end
