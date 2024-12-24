68cb256ebcaf21a69c80cc6ae79ca685711277b7
Mateusz Grotek <mateusz.grotek@digital.cabinet-office.gov.uk>
2024-12-24 13:56:22 +0000

    Run rails app:update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,8 +8,3 @@ Rails.application.config.assets.prefix = "/assets/static"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
